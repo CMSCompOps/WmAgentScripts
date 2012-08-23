11d95ce9d6d2650accb14398739edfeac93e87db
Xavier Janssen <xavier.janssen@cern.ch>
2012-08-23 08:05:16 +0000

    also remove UNDEFINED

--- condor_rm48h.sh
+++ condor_rm48h.sh
@@ -24,3 +24,19 @@ done < "$tmp"
 
 rm $tmp
 
+tmp=`mktemp`
+condor_overview | sed -n '/Jobs with RemoveReason!=UNDEFINED/,/-----/p' | grep " : " >> $tmp
+
+while read line ; do
+
+    WF=`echo "$line" | awk -F' : ' '{print $1}'`
+    Jobs=`echo "$line" | awk -F' : ' '{print $2}'`
+    echo "Checking : " $WF
+    nRem=0
+    for iJob in $Jobs ; do   
+      condor_rm -forcex $iJob
+      nRem=`expr $nRem + 1`
+    done
+done < "$tmp"
+
+rm $tmp
