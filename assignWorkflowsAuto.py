62e1f288c5198bfb62bd51da58165479479608cf
Andrew Lahiff <andrew.lahiff@stfc.ac.uk>
2012-09-17 15:33:24 +0000

    Handle PU_S0

--- assignWorkflowsAuto.py
+++ assignWorkflowsAuto.py
@@ -55,6 +55,8 @@ def getScenario(ps):
            pss = 'PU_S6'
         if ps == "process.mix=cms.EDProducer(MixingModule,mixProdStep1=cms.bool(False),mixProdStep2=cms.bool(False),maxBunch=cms.int32(3),useCurrentProcessOnly=cms.bool(False),LabelPlayback=cms.string(''),minBunch=cms.int32(-2),bunchspace=cms.int32(50),playback=cms.untracked.bool(False),input=cms.SecSource(PoolSource,nbPileupEvents=cms.PSet(histoFileName=cms.untracked.string('histProbFunction.root'),seed=cms.untracked.int32(54321),probFunctionVariable=cms.vint32(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24),probValue=cms.vdouble(0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0698146584,0.0630151648,0.0526654164,0.0402754482,0.0292988928,0.0194384503,0.0122016783,0.007207042,0.004003637,0.0020278322,0.0010739954,0.0004595759,0.0002229748,0.0001028162,4.5833715281e-05)),OOT_type=cms.untracked.string('Poisson'),":
            pss = 'PU_S4'
+        if ps == "process.mix=cms.EDProducer(MixingModule,mixProdStep1=cms.bool(False),mixProdStep2=cms.bool(False),maxBunch=cms.int32(0),useCurrentProcessOnly=cms.bool(False),LabelPlayback=cms.string(''),minBunch=cms.int32(0),bunchspace=cms.int32(450),playback=cms.untracked.bool(False),input=cms.SecSource(PoolSource,nbPileupEvents=cms.PSet(histoFileName=cms.untracked.string('histProbFunction.root'),seed=cms.untracked.int32(54321),probFunctionVariable=cms.vint32(0,1,2,3,4,5,6,7,8,9,10),probValue=cms.vdouble(0.145168,0.251419,0.251596,0.17943,0.1,0.05,0.02,0.01,0.005,0.002,0.001)),sequential=cms.untracked.bool(False),type=cms.string('probFunction'),":
+           pss = 'PU_S0'
 
         return pss
 
@@ -380,7 +382,7 @@ def main():
               else:
                  print 'Skipping workflow ',workflow
            else:
-              print 'Assigning ',workflow,' with ',era,procversion,lfn,siteUse,team,maxmergeevents,activity,maxRSS,maxVSize
+              print 'Would assign ',workflow,' with ',era,procversion,lfn,siteUse,team,maxmergeevents,activity,maxRSS,maxVSize
 
 	sys.exit(0)
 
