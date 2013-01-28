b6da9fa999fc62fe163d9a44f84da1fda8974881
Jacob Linacre <linacre@fnal.gov>
2013-01-28 21:28:42 +0000

    updated SoftTimeout to 46.4hr

--- assignWorkflowsAuto.py
+++ assignWorkflowsAuto.py
@@ -199,7 +199,7 @@ def assignRequest(url,workflow,team,site,era,procversion, activity, lfn, maxmerg
               #"Memory": 2300.0,
               #"SizePerEvent": 342110,
               #"TimePerEvent": 17.5,
-              "SoftTimeout": 171600,
+              "SoftTimeout": 167000,
               "GracePeriod": 300,
               "MaxMergeEvents": maxmergeevents,
 	      "maxRSS": maxRSS,
