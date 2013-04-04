a0cb0b750f7228a519ebd312363475b77f5caf04
Edgar Fajardo Hernandez <efajardo@cern.ch>
2013-04-04 12:42:56 +0000

    fix

--- copyLFNfromSite.sh
+++ copyLFNfromSite.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-CURL_URL="https://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$1_Buffer&protocol=srmv2&lfn=$2"
-
+if [[ "$1" == T1* ]]; then 	
+	CURL_URL="https://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$1_Buffer&protocol=srmv2&lfn=$2"
+else
+	CURL_URL="https://cmsweb.cern.ch/phedex/datasvc/xml/prod/lfn2pfn?node=$1&protocol=srmv2&lfn=$2"
+fi
+echo $CURL_URL
 file=$(curl -s --insecure -X GET $CURL_URL | awk -F"'" '{print $32}')
-lcg-cp -v $file file:////tmp/delete.me
+echo $file
+lcg-cp -v -n 1 $file file:////tmp/delete.me
