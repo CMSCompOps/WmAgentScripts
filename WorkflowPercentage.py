8383c84e4151faa9c080ec585175f66d50020a88
Julian Andres Badillo Rojas <jbadillo@vocms174.cern.ch>
2014-07-01 14:53:32 +0200

    count lumis

--- WorkflowPercentage.py
+++ WorkflowPercentage.py
@@ -2,7 +2,7 @@
 import json
 import urllib2,urllib, httplib, sys, re, os
 from xml.dom.minidom import getDOMImplementation
-import reqMgrClient
+import reqMgrClient, dbs3Client
 
 """
     Calculates event progress percentage of a given workflow,
@@ -14,22 +14,28 @@ import reqMgrClient
 
 
 
-def percentageCompletion(url, workflow, outputDataset, verbose=False):
+def percentageCompletion(url, workflow, outputDataset, verbose=False, checkLumis=False):
     """
     Calculates Percentage of completion for a given workflow
     taking a particular output dataset
+    if checkLumis is enabled, we get lumis instead.
     """
-   
-    inputEvents = reqMgrClient.getInputEvents(url, workflow)
-    outputEvents = reqMgrClient.getOutputEvents(url, workflow, outputDataset)
+    if checkLumis:
+        inputDataset = reqMgrClient.getInputDataSet(url, workflow)
+        inputEvents = dbs3Client.getLumiCountDataSet(inputDataset)
+        outputEvents = dbs3Client.getLumiCountDataSet(outputDataset)
+    else:
+        inputEvents = reqMgrClient.getInputEvents(url, workflow)
+        outputEvents = reqMgrClient.getOutputEvents(url, workflow, outputDataset)
     if inputEvents==0 or not inputEvents:
 	    return 0
     if not outputEvents:
         outputEvents = 0  
     if verbose:
+
         print outputDataset
-        print "Input events:", int(inputEvents)
-        print "Output events:", int(outputEvents)
+        print "Input", "lumis:" if checkLumis else "events:", int(inputEvents)
+        print "Output", "lumis:" if checkLumis else "events:", int(outputEvents)
 
     percentage=100.0*outputEvents/float(inputEvents)
     return percentage
@@ -37,18 +43,20 @@ def percentageCompletion(url, workflow, outputDataset, verbose=False):
     
 def main():
     args=sys.argv[1:]
-    if not len(args)==1:
-        print "usage:WorkflowPercentage.py workflowname"
+    if not 1 <= len(args) <= 2:
+        print "usage:WorkflowPercentage.py workflowname [-l]"
         sys.exit(0)
     workflow=args[0]
-
+    checkLumis = False
+    if len(args) == 2 and args[1] == '-l':
+        checkLumis = True
     url='cmsweb.cern.ch'
 
     #retrieve the output datasets
     outputDataSets=reqMgrClient.outputdatasetsWorkflow(url, workflow)    
 
     for dataset in outputDataSets:
-        perc = percentageCompletion(url, workflow, dataset, verbose=True)
+        perc = percentageCompletion(url, workflow, dataset, verbose=True, checkLumis=checkLumis)
         print dataset,"match:",perc,"%"
 
     sys.exit(0);
