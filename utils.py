0a22545be84fb91c10bb7bf7db2cf7a5224b586f
jean-roch <vlimant@cern.ch>
2015-04-07 16:37:28 +0200

    phedex delete request

--- utils.py
+++ utils.py
@@ -441,6 +441,19 @@ def approveSubscription(url, phedexid, nodes=None ):
         return True
     return result
 
+def makeDeleteRequest(url, site,datasets, comments, priority='low'):
+    dataXML = createXML(datasets)
+    params = { "node" : site,
+               "data" : dataXML,
+               "group": "DataOps",
+               "priority": priority,
+               "request_only":"y" ,
+               "delete":"y",
+               "comments":comments
+               }
+    response = phedexPost(url, "/phedex/datasvc/json/prod/subscribe", params)
+    return response
+
 def makeReplicaRequest(url, site,datasets, comments, priority='high',custodial='n'): # priority used to be normal
     dataXML = createXML(datasets)
     params = { "node" : site,"data" : dataXML, "group": "DataOps", "priority": priority,
