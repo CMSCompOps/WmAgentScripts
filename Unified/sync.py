7049df3a4c7b69c408c560d216ebed4e6842abc7
jean-roch <vlimant@cern.ch>
2015-04-09 02:36:34 +0200

    better synching

--- Unified/sync.py
+++ Unified/sync.py
@@ -11,12 +11,14 @@ if __name__ == "__main__":
         
     for wf in session.query(Workflow).all():
         if spec and spec not in wf.name: continue
-        if not wf.status in ['away']: continue
+        #if not wf.status in ['away']: continue
 
         wl = getWorkLoad('cmsweb.cern.ch', wf.name)
         wf.wm_status = wl['RequestStatus']
         if status:
             wf.status = status
+        elif wf.wm_status in ['assignment-approved']:
+            wf.status = 'considered'
         elif wf.wm_status in ['assigned','acquired','running-closed','running-open','completed','closed-out']:
             wf.status = 'away'
         elif wf.wm_status in ['rejected','failed','aborted','aborted-archived','rejected-archived','failed-archived']:
