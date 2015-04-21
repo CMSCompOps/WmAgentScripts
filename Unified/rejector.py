8dc8209cb415ecb8ab3f68c912fd2862d8f45fa0
jean-roch <vlimant@cern.ch>
2015-04-21 11:27:07 +0200

    options and clone option for rejector

--- Unified/rejector.py
+++ Unified/rejector.py
@@ -1,11 +1,13 @@
 from assignSession import *
 import sys
 import reqMgrClient
+import resubmit
 import setDatasetStatusDBS3
 from utils import getWorkLoad
+import optparse
 
-def rejector(url, specific ):
-
+def rejector(url, specific, options=None):
+    
     if specific.startswith('/'):
         pass
     else:
@@ -24,15 +26,31 @@ def rejector(url, specific ):
         
         datasets = wl['OutputDatasets']
         for dataset in datasets:
+            #oo = session.query(Output).filter(Output.datasetname==dataset).first()
+            #if oo:                oo.status = 'forget'
             results.append( setDatasetStatusDBS3.setStatusDBS3('https://cmsweb.cern.ch/dbs/prod/global/DBSWriter', dataset, 'INVALID', None) )
         if all(map(lambda result : result in ['None',None],results)):
             wfo.status = 'forget'
             session.commit()
             print wfo.name,"and",datasets,"are rejected"
+            if options and options.clone:
+                user = os.getenv('USER')
+                group = 'DATAOPS'
+                clone = resubmit.cloneWorkflow(wfo.name, user, group)
+                wfo.status = 'trouble'
+                session.commit()
         else:
             print "error in rejecting",wfo.name,results
 
 if __name__ == "__main__":
     url = 'cmsweb.cern.ch'
-    spec = sys.argv[1]
-    rejector(url, spec)
+
+    parser = optparse.OptionParser()
+    parser.add_option('-c','--clone',help="clone the workflow",default=False,action="store_true")
+    (options,args) = parser.parse_args()
+
+    spec=None
+    if len(args):
+        spec = args[0]
+    
+    rejector(url, spec, options)
