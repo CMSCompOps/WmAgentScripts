f5e8eebb8d3ed5c8431b5626674f26c9979b7165
jean-roch <vlimant@cern.ch>
2015-04-22 23:30:53 +0200

    logs to web, cleaned every 10 days, ownership alternates on week oddity

--- cWrap.sh
+++ cWrap.sh
@@ -1,8 +1,17 @@
 cd /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts
 
+week=`date +%W`
+let oddity=week%2
+if ( [ "$USER" = "vlimant" ] && [ "$oddity" = "1" ] ) || ( [ "$USER" = "mcremone" ] && [ "$oddity" = "0" ] ) ; then
+    echo no go for $USER on week $week
+    return
+fi
+
 export X509_USER_PROXY=$HOME/private/personal/voms_proxy.cert
 cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
 
 source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
 
-python $*
+log=/afs/cern.ch/user/c/cmst2/www/unified/logs/`echo $1 | sed 's/\.py//' | sed 's/Unified\///'`/`date +%F_%T`.log
+
+python $* > $log
---
+++ cleanlog.sh
@@ -0,0 +1,7 @@
+for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.log` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each
+	#rm -f $each
+    fi
+done
