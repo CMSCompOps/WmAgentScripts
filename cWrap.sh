32cc19939bcad92f67753dc2f59fce6897559336
jean-roch <vlimant@cern.ch>
2015-04-28 09:10:25 +0200

    ivnert week for closeout commissioning. date and user in log. last module run log

--- cWrap.sh
+++ cWrap.sh
@@ -2,7 +2,8 @@ cd /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts
 
 week=`date +%W`
 let oddity=week%2
-if ( [ "$USER" = "vlimant" ] && [ "$oddity" = "1" ] ) || ( [ "$USER" = "mcremone" ] && [ "$oddity" = "0" ] ) ; then
+
+if ( [ "$USER" = "vlimant" ] && [ "$oddity" = "0" ] ) || ( [ "$USER" = "mcremone" ] && [ "$oddity" = "1" ] ) ; then
     echo no go for $USER on week $week
     return
 fi
@@ -11,12 +12,16 @@ export X509_USER_PROXY=$HOME/private/personal/voms_proxy.cert
 cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
 
 source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
-
+    
 log=/afs/cern.ch/user/c/cmst2/www/unified/logs/`echo $1 | sed 's/\.py//' | sed 's/Unified\///'`/`date +%F_%T`.log
 
 echo `date` > $log
+echo $USER >> $log
+echo the week $week oddity is $oddity >> $log
+echo module `echo $1 | sed 's/\.py//' | sed 's/Unified\///'`>> $log 
 echo >> $log
 
 python $* >> $log
-
 cp $log /afs/cern.ch/user/c/cmst2/www/unified/logs/`echo $1 | sed 's/\.py//' | sed 's/Unified\///'`/last.log
+
+cp $log /afs/cern.ch/user/c/cmst2/www/unified/logs/last.log
