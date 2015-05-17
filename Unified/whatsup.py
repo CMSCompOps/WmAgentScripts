d2c5e5cefe428dc9951a84ebffc59fe2328953e7
jean-roch <vlimant@cern.ch>
2015-05-17 20:20:16 +0200

    give priorities

--- Unified/whatsup.py
+++ Unified/whatsup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from assignSession import *
 import time
+from utils import workflowInfo
 
 for wf in session.query(Workflow).filter(Workflow.status=='considered').all():
     print "workflow next in line to transfer:",wf.name
 print "\n"
 
 for wf in session.query(Workflow).filter(Workflow.status=='staging').all():
-    print "workflow held for staging:",wf.name
+    wfi = workflowInfo('cmsweb.cern.ch',wf.name,spec=False)
+    print "workflow held for staging:",wf.name,wfi.request['RequestPriority']
 print "\n"
 
 for wf in session.query(Workflow).filter(Workflow.status=='staged').all():
