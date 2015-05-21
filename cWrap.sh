0c04e95f5c6856c9575adce3bdad6f1c75d21190
jean-roch <vlimant@cern.ch>
2015-05-21 15:55:24 +0200

    factor out the credentials

--- cWrap.sh
+++ cWrap.sh
@@ -1,8 +1,5 @@
 cd /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts
 
-#talk to mcm
-cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o ~/private/prod-cookie.txt --krb
-
 week=`date +%W`
 let oddity=week%2
 
@@ -11,21 +8,26 @@ if ( [ "$USER" = "vlimant" ] && [ "$oddity" = "0" ] ) || ( [ "$USER" = "mcremone
     exit
 fi
 
-#talk to cmsweb
-export X509_USER_PROXY=$HOME/private/personal/voms_proxy.cert
-cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
-
-source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
-    
 log=/afs/cern.ch/user/c/cmst2/www/unified/logs/`echo $1 | sed 's/\.py//' | sed 's/Unified\///'`/`date +%F_%T`.log
 
 echo `date` > $log
 echo $USER >> $log
 echo the week $week oddity is $oddity >> $log
 echo module `echo $1 | sed 's/\.py//' | sed 's/Unified\///'`>> $log 
+
+source /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts/credentials.sh &>> $log
+
+source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
+
 echo >> $log
 
+###echo "Shutting things down !" >> $log
+
+#tlog=/tmp/`echo $1 | sed 's/\.py//' | sed 's/Unified\///'`/`date +%F_%T`.log
+#python $* &> $tlog
+#cat $tlog >> $log
 python $* &>> $log
+
 
 echo "finished" >> $log
 echo `date` >> $log
