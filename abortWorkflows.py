3e3eb0f9bf4870995f45bc92f4da504949731ce7
Julian Badillo <jbadillo@cern.ch>
2015-07-03 14:02:24 +0200

    Add parser options for single workflow or files

--- abortWorkflows.py
+++ abortWorkflows.py
@@ -13,20 +13,25 @@ except:
 import reqMgrClient
 
 def main():
-    """
-    Read the text file, for each workflow try:
-    Abort it.
-    """
-    args=sys.argv[1:]
-    if not len(args)==1:
-        print "usage:abortWorkflows file.txt"
-        sys.exit(0)
     url='cmsweb.cern.ch'
-    filename=args[0]
-    workflows = [wf.strip() for wf in open(filename).readlines() if wf.strip()]
-    for workflow in workflows:
-        print "Aborting workflow: " + workflow
-        reqMgrClient.abortWorkflow(url, workflow)
+    
+    #Create option parser
+    usage = "\n       python %prog [-f FILE_NAME | WORKFLOW_NAME ...]\n"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--file', help='Text file with a list of workflows', dest='file')
+    (options, args) = parser.parse_args()
+    
+    if options.file:
+        wfs = [l.strip() for l in open(options.file) if l.strip()]
+    elif args:
+        wfs = args
+    else:
+        parser.error("Provide the workflow of a file of workflows")
+        sys.exit(1)
+    
+    for wf in wfs:
+        print "Aborting workflow: " + wf
+        reqMgrClient.abortWorkflow(url, wf)
         print "Aborted"
     sys.exit(0);
 
