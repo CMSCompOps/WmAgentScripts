a5e312807c0d10c6ee5f2690798317b739de8cf7
AndrewLevin <relval@cmsrelval003.cern.ch>
2015-07-19 15:40:32 +0200

    put WMCore back into the pythonpath in set_environment.sh

--- RelVal/set_environment.sh
+++ RelVal/set_environment.sh
@@ -1,4 +1,4 @@
-#export PYTHONPATH=$PYTHONPATH:/home/relval/WMCore/src/python/
+export PYTHONPATH=$PYTHONPATH:/home/relval/WMCore/src/python/
 source /cvmfs/grid.cern.ch/emi-ui-3.7.3-1_sl6v2/etc/profile.d/setup-emi3-ui-example.sh
 export X509_USER_PROXY=/tmp/x509up_u13536
 source /home/relval/sw/comp.pre/slc6_amd64_gcc481/cms/dbs3-client/3.2.10-comp/etc/profile.d/init.sh;
