4c28b8784fc3437a5702fb3ba48630e9cdfd4d2d
AndrewLevin <relval@cmsrelval003.cern.ch>
2015-08-08 14:14:08 +0200

    fixed bug in input_dset_checkor.py

--- RelVal/input_dset_checkor.py
+++ RelVal/input_dset_checkor.py
@@ -123,7 +123,7 @@ def main():
                                     all_dsets_blocks_at_site=False
 
             if all_dsets_blocks_at_site and (not isthereanmcpileupdataset or ismcpileupdatasetatsite):
-                curs.execute("update batches set status=\"input_dsets_ready\", current_status_start_time=\""+datetime.datetime.now().strftime("%y:%m:%d %H:%M:%S")+"\" where useridyear = \""+ useridyear+"\" and useridmonth = \""+useridmonth+"\" and useridday = \""+useridday+"\" and useridnum = "+str(useridnum)+" and batch_version_num = "+str(batch_version_num)+";")
+                curs.execute("update batches set status=\"input_dsets_ready\", current_status_start_time=\""+datetime.datetime.now().strftime("%y:%m:%d %H:%M:%S")+"\" where useridyear = \""+ batch_dict["useridyear"]+"\" and useridmonth = \""+batch_dict["useridmonth"]+"\" and useridday = \""+batch_dict["useridday"]+"\" and useridnum = "+str(batch_dict["useridnum"])+" and batch_version_num = "+str(batch_dict["batch_version_num"])+";")
                 mysqlconn.commit()
 
         if batch_dict["status"] == "approved":
