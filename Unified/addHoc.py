1957545453829fb37712796b210dd545a47ef5c5
jean-roch <vlimant@cern.ch>
2015-10-13 13:12:42 +0200

    changes

--- Unified/addHoc.py
+++ Unified/addHoc.py
@@ -2,8 +2,13 @@
 from assignSession import *
 import os
 
+print "nothing add-Hoc to be done"
 
-for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-biglumi').all():
-    if 'Summer15' in wfo.name:
-        os.system('Unified/rejector.py --clone %s'% wfo.name)
+for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-manual').all():
+    if 'EXO-RunIIWinter15wmLHE' in wfo.name:
+        print "could reject it but would need the dsn"
+
+#for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-biglumi').all():
+#    if 'Summer15' in wfo.name:
+#        os.system('Unified/rejector.py --clone %s'% wfo.name)
     
