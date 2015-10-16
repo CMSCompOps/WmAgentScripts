678b71630632848fa8cf901938e5549dd00f61fb
jean-roch <vlimant@cern.ch>
2015-10-16 12:15:44 +0200

    add failed in bad statuses

--- Unified/lockor.py
+++ Unified/lockor.py
@@ -40,32 +40,38 @@ for status in reversed(statuses):
             newly_locking.add(dataset)
     print len(newly_locking),"locks so far"
 
-## check on the one left out
+## check on the one left out, which would seem to get unlocked
 for dataset in already_locked-newly_locking:
-    ds_status = getDatasetStatus( dataset )
-    if ds_status in ['INVALID']: 
-        ## don't even try to keep the lock
-        print "\tunlocking",dataset,"for bad dataset status",ds_status
-        continue
-
+    unlock=False
     creators = getWorkflowByOutput( url, dataset , details=True)
     creators_status = [r['RequestStatus'] for r in creators]
     print "Statuses of workflow that made the dataset",creators_status
-    if all(status in ['aborted','rejected','aborted-archived','rejected-archived'] for status in creators_status):
+    if all(status in ['failed','aborted','rejected','aborted-archived','rejected-archived'] for status in creators_status):
         ## crap 
         print "\tunlocking",dataset,"for bad workflow statuses"
-        continue
+        unlock=True
 
-    relock=False
-    (_,dsn,ps,tier) = dataset.split('/')
-    if not tier in tier_no_custodial:
-        custodials = findCustodialLocation(url, dataset)
-        if len(custodials) == 0:
-            print "Can't unlock",dataset,"because it is not custodial yet",ds_status
-            ## add it back for that reason
-            newly_locking.add(dataset)
-            relock = True
-    if not relock:
+    ds_status=None
+    if not unlock:
+        ds_status = getDatasetStatus( dataset )
+        if ds_status in ['INVALID']: 
+            ## don't even try to keep the lock
+            print "\tunlocking",dataset,"for bad dataset status",ds_status
+            unlock=True
+
+    if not unlock:
+        (_,dsn,ps,tier) = dataset.split('/')
+        unlock = True
+        if not tier in tier_no_custodial:
+            custodials = findCustodialLocation(url, dataset)
+            if len(custodials) == 0:
+                if not ds_status: ds_status = getDatasetStatus( dataset )
+                print "Can't unlock",dataset,"because it is not custodial yet",ds_status
+                ## add it back for that reason
+                newly_locking.add(dataset)
+                unlock = False
+
+    if unlock:
         print "\tunlocking",dataset
         ##would like to pass to *-unlock, or even destroy from local db
         for creator in creators:
@@ -77,3 +83,21 @@ for dataset in already_locked-newly_locking:
             
 open('/afs/cern.ch/user/c/cmst2/www/unified/globallocks.json.new','w').write( json.dumps( list(newly_locking), indent=2))
 os.system('mv /afs/cern.ch/user/c/cmst2/www/unified/globallocks.json.new /afs/cern.ch/user/c/cmst2/www/unified/globallocks.json')
+
+
+## now settting the statuses locally right
+## this below does not function because you cannot -unlock the workflow that uses the dataset in input. 
+# so doing it by output only, as above, is enough and sufficient.
+#for dataset in unlocking:
+#    pass
+    ## if a dataset goes out of lock, you want to unlock everything about it
+    #actors = getWorkflowByOutput( url, dataset , details=True)
+    #actors.extend( getWorkflowByInput( url, dataset , details=True))
+    ## adding secondaries ???
+    #actors.extend( getWorkflowByPileup( url, dataset , details=True))
+    #for actor in actors:
+    #    for wfo in  session.query(Workflow).filter(Workflow.name==actor['RequestName']).all():
+    #        if not 'unlock' in wfo.status:
+    #            wfo.status +='-unlock'
+    #            print "setting",wfo.name,"to",wfo.status
+    #session.commit()
