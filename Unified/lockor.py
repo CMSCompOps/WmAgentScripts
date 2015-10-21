bbe59b121abc77b3e4f11a0c3d67964f8bc2b862
jean-roch <vlimant@cern.ch>
2015-10-21 18:41:28 +0200

    made robust

--- Unified/lockor.py
+++ Unified/lockor.py
@@ -29,7 +29,9 @@ for status in reversed(statuses):
     print len(wfls),"in",status
     for wl in wfls:
         ## unknonw to the system
-        if not session.query(Workflow).filter(Workflow.name==wl['RequestName']).all(): continue
+        if not session.query(Workflow).filter(Workflow.name==wl['RequestName']).all(): 
+            #print wl['RequestName'],"is unknown, this is bad news" ## no it is not
+            continue
 
         wfi = workflowInfo( url,  wl['RequestName'], request = wl ,spec=False)
         (_,primaries,_,secondaries) = wfi.getIO()
@@ -42,44 +44,49 @@ for status in reversed(statuses):
 
 ## check on the one left out, which would seem to get unlocked
 for dataset in already_locked-newly_locking:
-    unlock=False
-    creators = getWorkflowByOutput( url, dataset , details=True)
-    creators_status = [r['RequestStatus'] for r in creators]
-    print "Statuses of workflow that made the dataset",creators_status
-    if all(status in ['failed','aborted','rejected','aborted-archived','rejected-archived'] for status in creators_status):
-        ## crap 
-        print "\tunlocking",dataset,"for bad workflow statuses"
-        unlock=True
-
-    ds_status=None
-    if not unlock:
-        ds_status = getDatasetStatus( dataset )
-        if ds_status in ['INVALID']: 
-            ## don't even try to keep the lock
-            print "\tunlocking",dataset,"for bad dataset status",ds_status
+    try:
+        unlock=False
+        creators = getWorkflowByOutput( url, dataset , details=True)
+        creators_status = [r['RequestStatus'] for r in creators]
+        print "Statuses of workflow that made the dataset",creators_status
+        if all(status in ['failed','aborted','rejected','aborted-archived','rejected-archived'] for status in creators_status):
+            ## crap 
+            print "\tunlocking",dataset,"for bad workflow statuses"
             unlock=True
 
-    if not unlock:
-        (_,dsn,ps,tier) = dataset.split('/')
-        unlock = True
-        if not tier in tier_no_custodial:
-            custodials = findCustodialLocation(url, dataset)
-            if len(custodials) == 0:
-                if not ds_status: ds_status = getDatasetStatus( dataset )
-                print "Can't unlock",dataset,"because it is not custodial yet",ds_status
-                ## add it back for that reason
-                newly_locking.add(dataset)
-                unlock = False
+        ds_status=None
+        if not unlock:
+            ds_status = getDatasetStatus( dataset )
+            if ds_status in ['INVALID']: 
+                ## don't even try to keep the lock
+                print "\tunlocking",dataset,"for bad dataset status",ds_status
+                unlock=True
 
-    if unlock:
-        print "\tunlocking",dataset
-        ##would like to pass to *-unlock, or even destroy from local db
-        for creator in creators:
-            for wfo in  session.query(Workflow).filter(Workflow.name==creator['RequestName']).all():
-                if not 'unlock' in wfo.status:
-                    wfo.status +='-unlock'
-                    print "setting",wfo.name,"to",wfo.status
-        session.commit()
+        if not unlock:
+            (_,dsn,ps,tier) = dataset.split('/')
+            unlock = True
+            if not tier in tier_no_custodial:
+                custodials = findCustodialLocation(url, dataset)
+                if len(custodials) == 0:
+                    if not ds_status: ds_status = getDatasetStatus( dataset )
+                    print "Can't unlock",dataset,"because it is not custodial yet",ds_status
+                    ## add it back for that reason
+                    newly_locking.add(dataset)
+                    unlock = False
+
+        if unlock:
+            print "\tunlocking",dataset
+            ##would like to pass to *-unlock, or even destroy from local db
+            for creator in creators:
+                for wfo in  session.query(Workflow).filter(Workflow.name==creator['RequestName']).all():
+                    if not 'unlock' in wfo.status and not wfo.status in ['trouble','away']:
+                        wfo.status +='-unlock'
+                        print "setting",wfo.name,"to",wfo.status
+            session.commit()
+    except Exception as e:
+        print "Error in checking unlockability. relocking",dataset
+        print str(e)
+        newly_locking.add(dataset)
             
 open('/afs/cern.ch/user/c/cmst2/www/unified/globallocks.json.new','w').write( json.dumps( list(newly_locking), indent=2))
 os.system('mv /afs/cern.ch/user/c/cmst2/www/unified/globallocks.json.new /afs/cern.ch/user/c/cmst2/www/unified/globallocks.json')
