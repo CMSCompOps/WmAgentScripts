96621a3d7c18cd6ff1806458e5b94b40feb967ea
Julian Badillo <jbadillo@cern.ch>
2015-11-06 17:16:35 +0100

    keep track of makeACDC's changes

--- makeACDC.py
+++ makeACDC.py
@@ -4,7 +4,8 @@
     Use: The workflow name and the initial task name.
     It will copy all the original workflow parameters unless specified
 """
-import logging, json, sys
+import logging
+import sys
 from optparse import OptionParser
 from reqmgr import ReqMgrClient
 logging.basicConfig(level=logging.WARNING)
@@ -20,12 +21,46 @@ class Config:
         self.changeSplitting = False
         self.assignRequest = False
 
+url = 'https://cmsweb.cern.ch'
+testbed_url = 'https://cmsweb-testbed.cern.ch'
+#url = 'https://alan-cloud1.cern.ch'
+    
+configJson = {"createRequest":{}}
+config = Config(configJson)
+reqMgrClient = ReqMgrClient(url, config)
+    
+def makeACDC(url, workflow, task, memory=None):
+    #original wf info
+    wf = rqMgr.Workflow(workflow)
+    
+    #set up acdc stuff
+    if "ACDC" in wf.info["RequestString"]:
+        config.requestArgs["createRequest"]["RequestString"] = wf.info["RequestString"]
+    else:
+        config.requestArgs["createRequest"]["RequestString"] = "ACDC_"+ wf.info["RequestString"]
+    
+    config.requestArgs["createRequest"]["PrepID"] = wf.info["PrepID"]
+    config.requestArgs["createRequest"]["RequestPriority"] = wf.info["RequestPriority"]
+    config.requestArgs["createRequest"]["OriginalRequestName"] = wf.name
+    config.requestArgs["createRequest"]["InitialTaskPath"] = "/%s/%s"%(wf.name, task)
+    config.requestArgs["createRequest"]["ACDCServer"] = "https://cmsweb.cern.ch/couchdb"
+    config.requestArgs["createRequest"]["ACDCDatabase"] = "acdcserver"
+    config.requestArgs["createRequest"]["TimePerEvent"] = wf.info["TimePerEvent"]
+    
+    if memory:
+        config.requestArgs["createRequest"]["Memory"] = memory
+    else:
+        config.requestArgs["createRequest"]["Memory"] = wf.info["Memory"]
+    
+    config.requestArgs["createRequest"]["SizePerEvent"] = wf.info["SizePerEvent"]
+    config.requestArgs["createRequest"]["RequestType"] = "Resubmission"
+    config.requestArgs["createRequest"]["Group"] = wf.info["Group"]
+    
+    r = reqMgrClient.createRequest(config)
+    return r
+
 
 def main():
-
-    url = 'https://cmsweb.cern.ch'
-    testbed_url = 'https://cmsweb-testbed.cern.ch'
-    #url = 'https://alan-cloud1.cern.ch'
 
     #Create option parser
     usage = "usage: %prog [options] [WORKFLOW] TASK"
@@ -49,36 +84,8 @@ def main():
         parser.error("Provide the Workflow Name and the Task Name")
         sys.exit(1)
 
-    configJson = {"createRequest":{}}
-    config = Config(configJson)
-    reqMgrClient = ReqMgrClient(url, config)
-    
     for wfname in wfs:
-        #original wf info
-        wf = rqMgr.Workflow(wfname)
-        #set up acdc stuff
-        if "ACDC" in wf.info["RequestString"]:
-            config.requestArgs["createRequest"]["RequestString"] = wf.info["RequestString"]
-        else:
-            config.requestArgs["createRequest"]["RequestString"] = "ACDC_"+ wf.info["RequestString"]
-        config.requestArgs["createRequest"]["PrepID"] = wf.info["PrepID"]
-        config.requestArgs["createRequest"]["RequestPriority"] = wf.info["RequestPriority"]
-        config.requestArgs["createRequest"]["OriginalRequestName"] = wf.name
-        config.requestArgs["createRequest"]["InitialTaskPath"] = "/%s/%s"%(wf.name, task)
-        config.requestArgs["createRequest"]["ACDCServer"] = "https://cmsweb.cern.ch/couchdb"
-        config.requestArgs["createRequest"]["ACDCDatabase"] = "acdcserver"
-        config.requestArgs["createRequest"]["TimePerEvent"] = wf.info["TimePerEvent"]
-
-        if options.memory:
-            config.requestArgs["createRequest"]["Memory"] = options.memory
-        else:
-            config.requestArgs["createRequest"]["Memory"] = wf.info["Memory"]
-
-        config.requestArgs["createRequest"]["SizePerEvent"] = wf.info["SizePerEvent"]
-        config.requestArgs["createRequest"]["RequestType"] = "Resubmission"
-        config.requestArgs["createRequest"]["Group"] = wf.info["Group"]
-
-        r = reqMgrClient.createRequest(config)
+        r = makeACDC(url, wfname, task, options.memory)
         print "Created:"    
         print r
 
