40ac75c9efa23367848707b6f72d9c28565395e8
jean-roch <vlimant@cern.ch>
2015-12-07 10:36:22 +0100

    force acquire above 100k

--- Unified/addHoc.py
+++ Unified/addHoc.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python  
+from utils import workflowInfo
 from assignSession import *
 import os
+import sys
+
+url = 'cmsweb.cern.ch'
 
 print "nothing add-Hoc to be done"
+sys.exit(0)
 
-for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-manual').all():
-    if 'EXO-RunIIWinter15wmLHE' in wfo.name:
-        print "could reject it but would need the dsn"
+for wfo in session.query(Workflow).filter(Workflow.status == 'staging').all():
+    wfi = workflowInfo(url, wfo.name )
+    if wfi.request['RequestPriority'] < 100000 : continue
+    print "forcing acquiring from staging",wfo.name
+    os.system('Unified/assignor.py --go  %s'% wfo.name)
+
+for wfo in session.query(Workflow).filter(Workflow.status == 'staged').all():
+    wfi = workflowInfo(url, wfo.name )
+    if wfi.request['RequestPriority'] < 100000 : continue
+    print "forcing acquiring from staged",wfo.name
+    os.system('Unified/assignor.py --go  %s'% wfo.name)
+
+
+
+#for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-manual').all():
+#    if 'EXO-RunIIWinter15wmLHE' in wfo.name:
+#        print "could reject it but would need the dsn"
 
 #for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-biglumi').all():
 #    if 'Summer15' in wfo.name:
