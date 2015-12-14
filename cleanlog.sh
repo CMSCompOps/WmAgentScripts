07770a296b15eb447fb1264615d90a29b67213a1
jean-roch <vlimant@cern.ch>
2015-12-14 10:48:13 +0100

    clean timing log too

--- cleanlog.sh
+++ cleanlog.sh
@@ -5,3 +5,10 @@ for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.log` ; do
 	rm -f $each
     fi
 done
+for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.time` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each
+	rm -f $each
+    fi
+done
