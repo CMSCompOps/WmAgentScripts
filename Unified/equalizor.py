7958b789b71702630f90b626b8e54e77f366daba
jean-roch <vlimant@cern.ch>
2015-12-15 13:42:25 +0100

    good version of equalizer, with not too many rules

--- Unified/equalizor.py
+++ Unified/equalizor.py
@@ -22,6 +22,7 @@ def equalizor(url , specific = None):
     SI = siteInfo()
     for site in SI.sites_ready:
         region = site.split('_')[1]
+        if not region in ['US','IT']: continue
         regions[region] = [region] 
 
     for site in SI.sites_ready:
@@ -46,7 +47,7 @@ def equalizor(url , specific = None):
         task_name = task.pathName.split('/')[-1]
         running, idled = running_idle( wfi, task_name)
         if not idled and not running : return False, task_name, running, idled
-        if (not running and idled) or (idled / float(running) > 0.3):
+        if (not running and idled) or (idled / float(running) > 0.2):
             return True, task_name, running, idled
         else:
             return False, task_name, running, idled
@@ -76,9 +77,9 @@ def equalizor(url , specific = None):
 
         ## now parse this for action
         for task,campaign in tasks_and_campaigns:
-            #print task.pathName
-            #print campaign
-            if campaign == 'RunIIWinter15wmLHE':
+            print task.pathName
+            print campaign
+            if campaign in [ 'RunIIWinter15wmLHE', 'RunIISummer15GS']:
                 if task.taskType == 'Processing':
                     needs, task_name, running, idled = needs_action(wfi, task)
                     if needs:
@@ -86,7 +87,7 @@ def equalizor(url , specific = None):
                         set_to = wfi.request['SiteWhitelist']
                         modifications[wfo.name][task.pathName] = { "ReplaceSiteWhitelist" : set_to ,"Running" : running, "Pending" : idled}
                     else:
-                        print task_name,"of",wfo.name,"running",running,"and pending"
+                        print task_name,"of",wfo.name,"running",running,"and pending",idled
             if campaign == 'RunIIFall15DR76':
                 ## we should add all sites that hold the secondary input if any
                 secondary_locations = ['T1_ES_PIC','T2_US_Purdue','T2_UK_SGrid_RALPP','T2_BE_IIHE','T2_DE_DESY','T2_IT_Legnaro','T2_US_Caltech','T1_DE_KIT','T2_UK_London_Brunel','T2_IT_Pisa','T1_US_FNAL','T2_IT_Rome','T2_US_Florida','T1_IT_CNAF','T1_RU_JINR','T2_UK_London_IC','T2_US_Nebraska','T2_FR_CCIN2P3','T2_US_UCSD','T2_ES_CIEMAT','T1_FR_CCIN2P3','T2_US_Wisconsin','T2_US_MIT','T2_DE_RWTH','T1_UK_RAL','T2_US_Vanderbilt','T2_CH_CERN']
@@ -103,9 +104,12 @@ def equalizor(url , specific = None):
                         print task_name,"of",wfo.name,"running",running,"and pending",idled,"taking action : AddWhitelist"
                     else:
                         print task_name,"of",wfo.name,"running",running,"and pending",idled
-    interface = {'mapping' : mapping, 
-                 'reversed_mapping' : reversed_mapping,
-                 'modifications' : {}}
+    interface = {
+        #'mapping' : mapping, 
+        'reversed_mapping' : reversed_mapping,
+        #'acting_on' : ['RunIIWinter15wmLHE'],
+        'modifications' : {}
+        }
     if options.augment:
         interface['modifications'] = json.loads( open('/afs/cern.ch/user/c/cmst2/www/unified/equalizor.json').read())['modifications']
     interface['modifications'].update( modifications )
---
+++ Unified/go_condor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import re
+import sys
+import json
+import classad
+from collections import defaultdict
+
+SITE_RE = re.compile("T[012]_[A-Z]+_[A-Za-z0-9_]+")
+WORKFLOW_RE = re.compile("[-_A-Za-z0-9]+")
+
+config = json.load(sys.stdin)
+
+reversed_mapping = config['reversed_mapping']
+
+needs_site = defaultdict(set)
+for workflow, tasks in config['modifications'].items():
+    for taskname,specs in tasks.items():
+        anAd = classad.ClassAd()
+        anAd["GridResource"] = "condor localhost localhost"
+        anAd["TargetUniverse"] = 5
+        exp = 'regexp(target.WMAgent_SubTaskName, "%s")'% (taskname)
+        anAd["Requirements"] = classad.ExprTree(str(exp))
+        
+        if "ReplaceSiteWhitelist" in specs:
+            anAd["Name"] = str("Site Replacement for %s"% taskname)
+            anAd["eval_set_DESIRED_Sites"] = str(",".join(specs['ReplaceSiteWhitelist']))
+            print anAd
+        elif "AddWhitelist" in specs:
+            for site in specs['AddWhitelist']:
+                needs_site[site].add(taskname)
+ 
+
+for site in  needs_site:
+    if not site in reversed_mapping: continue
+    anAd = classad.ClassAd()
+    anAd["GridResource"] = "condor localhost localhost"
+    anAd["TargetUniverse"] = 5
+    anAd["Name"] = str("Overflow rule for %s"%site)
+    anAd["OverflowTasknames"] = map(str, needs_site[site])
+    #anAd["OverflowSite"] = str(site)
+    exp = classad.ExprTree('regexp("%s", ExtDESIRED_Sites) && member(target.WMAgent_SubTaskName, OverflowTasknames)'%str(site))
+    anAd["Requirements"] = classad.ExprTree(str(exp))
+    anAd["eval_set_DESIRED_Sites"] = classad.Function("strcat", str(",".join( reversed_mapping[site]+[''] )), classad.Attribute("ExtDESIRED_Sites"))
+    print anAd
