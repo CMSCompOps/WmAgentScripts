8d2afa3aefbae2cbb8b1120de387c943772c98c4
jean-roch <vlimant@cern.ch>
2016-01-06 16:29:52 +0100

    fix for change of year bug

--- Unified/messagor.py
+++ Unified/messagor.py
@@ -5,15 +5,18 @@ from collections import defaultdict
 import time
 
 this_week = int(time.strftime("%W",time.gmtime()))
-start_time_two_weeks_ago = time.mktime(time.strptime("15-0-%d"%(this_week-3), "%y-%w-%W"))
-subject="Ouput Produced on Week %d"%(this_week-1)
+#start_time_two_weeks_ago = time.mktime(time.strptime("15-0-%d"%(this_week-3), "%y-%w-%W"))
+now = time.mktime(time.gmtime())
+start_time_two_weeks_ago = time.mktime(time.gmtime(now - (15*24*60*60))) # 14+1 days ago
+last_week = int(time.strftime("%W",time.gmtime( now - (7*24*60*60))))
+subject="Ouput Produced on Week %d"%(last_week)
 
 ds=defaultdict(list)
 n_produced=0
 for out in session.query(Output).filter(Output.date>=start_time_two_weeks_ago).all():
     if  out.workflow.status in ['done-unlock','done','clean','clean-out','clean-unlock']:
         out_week = int(time.strftime("%W",time.gmtime(out.date)))
-        if (this_week-out_week)==1:
+        if last_week==out_week:
             (_,_,_,tier) = out.datasetname.split('/')
             ds[tier].append( out.datasetname )
             n_produced+=1
