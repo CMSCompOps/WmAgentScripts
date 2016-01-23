3eda72934f47fb35243aa798946b65ca89550257
jean-roch <vlimant@cern.ch>
2016-01-23 07:43:59 +0100

    refactor of ddm injection. check on full copy for announcing

--- Unified/closor.py
+++ Unified/closor.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 from assignSession import *
-from utils import getWorkLoad
-from utils import componentInfo, sendEmail, setDatasetStatus, unifiedConfiguration
+from utils import componentInfo, sendEmail, setDatasetStatus, unifiedConfiguration, workflowInfo
 import reqMgrClient
 import json
 import time
 import sys
-import subprocess
-from utils import getDatasetEventsAndLumis, campaignInfo, getDatasetPresence
-#from utils import lockInfo
+from utils import getDatasetEventsAndLumis, campaignInfo, getDatasetPresence, findLateFiles
 from htmlor import htmlor
 from collections import defaultdict
 
@@ -19,37 +16,38 @@ def closor(url, specific=None):
     CI = campaignInfo()
     #LI = lockInfo()
 
+    all_late_files = []
     ## manually closed-out workflows should get to close with checkor
     for wfo in session.query(Workflow).filter(Workflow.status=='close').all():
 
         if specific and not specific in wfo.name: continue
 
         ## what is the expected #lumis 
-        wl = getWorkLoad(url, wfo.name)
-        wfo.wm_status = wl['RequestStatus']
+        wfi = workflowInfo(url, wfo.name )
+        wfo.wm_status = wfi.request['RequestStatus']
 
-        if wl['RequestStatus'] in  ['announced','normal-archived']:
+        if wfi.request['RequestStatus'] in  ['announced','normal-archived']:
             ## manually announced ??
             wfo.status = 'done'
-            wfo.wm_status = wl['RequestStatus']
+            wfo.wm_status = wfi.request['RequestStatus']
             print wfo.name,"is announced already",wfo.wm_status
 
         session.commit()
 
 
         expected_lumis = 1
-        if not 'TotalInputLumis' in wl:
+        if not 'TotalInputLumis' in wfi.request:
             print wfo.name,"has not been assigned yet, or the database is corrupted"
         else:
-            expected_lumis = wl['TotalInputLumis']
+            expected_lumis = wfi.request['TotalInputLumis']
 
         ## what are the outputs
-        outputs = wl['OutputDatasets']
+        outputs = wfi.request['OutputDatasets']
         ## check whether the number of lumis is as expected for each
         all_OK = defaultdict(lambda : False)
         #print outputs
         if len(outputs): 
-            print wfo.name,wl['RequestStatus']
+            print wfo.name,wfi.request['RequestStatus']
         for out in outputs:
             event_count,lumi_count = getDatasetEventsAndLumis(dataset=out)
             odb = session.query(Output).filter(Output.datasetname==out).first()
@@ -85,11 +83,20 @@ def closor(url, specific=None):
                 print out,"is in full at",",".join(where)
             else:
                 print out,"is not in full anywhere"
-                print json.dumps( presence, indent=2 )
+                going_to = wfi.request['NonCustodialSites']+wfi.request['CustodialSites']
+                print 'send to',','.join(going_to)
+                at_destination = dict([(k,v) for (k,v) in presence.items() if k in going_to])
+                else_where = dict([(k,v) for (k,v) in presence.items() if not k in going_to])
+                print json.dumps( at_destination )
+                print json.dumps( else_where, indent=2 )
+                ## do the full stuck transfer study, missing files and shit !
+                for there in going_to:
+                    late_info = findLateFiles(url, out, going_to = there )
+                    all_late_files.extend( late_info )
                 all_OK[out] = False
 
         ## only that status can let me go into announced
-        if all(all_OK.values()) and wl['RequestStatus'] in ['closed-out']:
+        if all(all_OK.values()) and wfi.request['RequestStatus'] in ['closed-out']:
             print wfo.name,"to be announced"
             results=[]#'dummy']
             if not results:
@@ -101,8 +108,8 @@ def closor(url, specific=None):
                         try:
                             campaign = out.split('/')[2].split('-')[0]
                         except:
-                            if 'Campaign' in wl and wl['Campaign']:
-                                campaign = wl['Campaign']
+                            if 'Campaign' in wfi.request and wfi.request['Campaign']:
+                                campaign = wfi.request['Campaign']
                         to_DDM = False
                         ## campaign override
                         if campaign and campaign in CI.campaigns and 'toDDM' in CI.campaigns[campaign] and tier in CI.campaigns[campaign]['toDDM']:
@@ -139,13 +146,17 @@ def closor(url, specific=None):
                         ## inject to DDM when necessary
                         if to_DDM:
                             #print "Sending",out," to DDM"
-                            status = subprocess.call(['python','assignDatasetToSite.py','--nCopies=%d'%n_copies,'--dataset='+out,'--exec', destination_spec])
-                            if status!=0:
+                            p = os.popen('python assignDatasetToSite.py --nCopies=%d --dataset=%s %s --exec'%(n_copies, out,destination_spec))
+                            print p.read()
+                            status = p.close()
+                            if status!=None:
                                 print "Failed DDM, retrying a second time"
-                                status = subprocess.call(['python','assignDatasetToSite.py','--nCopies=%d'%n_copies,'--dataset='+out,'--exec', destination_spec])
-                                if status!=0:
-                                    results.append("Failed DDM for %s"% out)
+                                p = os.popen('python assignDatasetToSite.py --nCopies=%d --dataset=%s %s --exec'%(n_copies, out,destination_spec))
+                                print p.read()
+                                status = p.close()    
+                                if status!=None:
                                     sendEmail("failed DDM injection","could not add "+out+" to DDM pool. check closor logs.")
+                            results.append( status )
                     else:
                         print wfo.name,"no stats for announcing",out
                         results.append('No Stats')
@@ -155,10 +166,10 @@ def closor(url, specific=None):
                     res = reqMgrClient.announceWorkflowCascade(url, wfo.name)
                     if not res in ['None',None]:
                         ## check the status again, it might well have toggled
-                        wl_bis = getWorkLoad(url, wfo.name)
-                        wfo.wm_status = wl_bis['RequestStatus']
-
-                        if wl_bis['RequestStatus'] in  ['announced','normal-archived']:
+                        wl_bis = workflowInfo(url, wfo.name)
+                        wfo.wm_status = wl_bis.request['RequestStatus']
+                        session.commit()
+                        if wl_bis.request['RequestStatus'] in  ['announced','normal-archived']:
                             res = None
                         else:
                             ## retry ?
@@ -174,9 +185,20 @@ def closor(url, specific=None):
             else:
                 print "ERROR with ",wfo.name,"to be announced",json.dumps( results )
         else:
-            print wfo.name,"not good for announcing:",wl['RequestStatus']
-                
+            print wfo.name,"not good for announcing:",wfi.request['RequestStatus']
 
+    days_late = 0.
+    retries_late = 10
+
+    really_late_files = [info for info in all_late_files if info['retries']>=retries_late]
+    really_late_files = [info for info in really_late_files if info['delay']/(60*60*24.)>=days_late]
+
+    if really_late_files:
+        sendEmail('waiting for files to announce','These files are lagging for %d days and %d retries announcing dataset \n%s'%(days_late, retries_late, json.dumps( really_late_files , indent=2)))
+        open('/afs/cern.ch/user/c/cmst2/www/unified/stuck_files.json','w').write( json.dumps( really_late_files , indent=2))
+
+
+        
 if __name__ == "__main__":
     url = 'cmsweb.cern.ch'
     spec=None
