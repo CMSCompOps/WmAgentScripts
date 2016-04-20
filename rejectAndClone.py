1aac2d486b8482de9f2db7cd9ad0d04cc322a52e
vlimant <vlimant@cern.ch>
2016-04-20 23:52:23 +0200

    do not point at the testbed by default

--- rejectAndClone.py
+++ rejectAndClone.py
@@ -18,7 +18,7 @@ import dbs3Client as dbs3
 from optparse import OptionParser
 
 
-url = 'cmsweb-testbed.cern.ch'
+url = 'cmsweb.cern.ch'
 def main():
     """
     Read the text file, for each workflow try:
