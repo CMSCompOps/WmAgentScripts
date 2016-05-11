0bd98c30838f1bb4eabcee5d50871c9b4ed52bc3
Paola Rozo <katherine.rozo@cern.ch>
2016-05-11 16:48:51 +0200

    Merging the rejecting scripts(rejectWorkflows.py,abortWorkflows.py,rejectAndClone.py and abortAndClone.p) and the resubmiting ones (resubmit.py and extendWorflow.py)

--- reject.py
+++ reject.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 """
     __author__ = "Paola Rozo"
+<<<<<<< 77af5a2f6c38937fe6412a64d5203d1052f5f53b
     __version__ = "0.2"
     __maintainer__ = "Paola Rozo"
     __email__ = "katherine.rozo@cern.ch"
 
     The script rejects or aborts a workflow or a set of workflows according to their status.
+=======
+    __version__ = "0.1"
+    __maintainer__ = "Paola Rozo"
+    __email__ = "katherine.rozo@cern.ch"
+    __status__ = "Testing"
+
+    The script rejects or aborts a workflow or a set of worflows according to their status.
+>>>>>>> Merging the rejecting scripts(rejectWorkflows.py,abortWorkflows.py,rejectAndClone.py and abortAndClone.p) and the resubmiting ones (resubmit.py and extendWorflow.py)
     The workflow can be cloned if the -c option is given
 """
 
@@ -20,6 +29,7 @@ url = 'cmsweb.cern.ch'
 
 
 def main():
+<<<<<<< 77af5a2f6c38937fe6412a64d5203d1052f5f53b
     usage = "\n       python %prog [options] [WORKFLOW_NAME]\n" \
             "WORKFLOW_NAME: if the list file is provided this should be empty\n"
 
@@ -29,10 +39,23 @@ def main():
     parser.add_option('-i', '--invalidate', help='Invalidate datasets? The default value is False',action="store_true", dest='invalidate', default=False)
     parser.add_option("-u", "--user", dest="user",help="The user for creating the clone, if empty it will use the OS user running the script")
     parser.add_option("-g", "--group", dest="group", default='DATAOPS',help="The group for creating the clone, if empty it will, use 'DATAOPS' by default")
+=======
+    usage = "\n       python %prog [options] [WORKFLOW_NAME] [USER GROUP]\n" \
+            "WORKFLOW_NAME: if the list file is provided this should be empty\n" \
+            "USER: the user for creating the clone, if empty it will\n" \
+            "      use the OS user running the script\n" \
+            "GROUP: the group for creating the clone, if empty it will\n" \
+            "      use 'DATAOPS' by default"
+
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--file', help='Text file of workflows to Reject and Clone', dest='file')
+    parser.add_option('-c', '--clone', help='The worflows need to be cloned: values:true or false', dest='clone')
+>>>>>>> Merging the rejecting scripts(rejectWorkflows.py,abortWorkflows.py,rejectAndClone.py and abortAndClone.p) and the resubmiting ones (resubmit.py and extendWorflow.py)
     (options, args) = parser.parse_args()
 
     # Check the arguments, get info from them
     if options.file:
+<<<<<<< 77af5a2f6c38937fe6412a64d5203d1052f5f53b
         try:
             workflows = [l.strip() for l in open(options.file) if l.strip()]
         except:
@@ -74,6 +97,56 @@ def main():
         if options.clone:
             print("Cloning workflow: "+ workflow)
             cloned = resubmit.cloneWorkflow(workflow, user, options.group)
+=======
+        wfs = [l.strip() for l in open(options.file) if l.strip()]
+        if len(args) == 2:
+            user = args[0]
+            group = args[1]
+        elif len(args) == 0:
+            # get os username by default
+            uinfo = pwd.getpwuid(os.getuid())
+            user = uinfo.pw_name
+            # group by default DATAOPS
+            group = 'DATAOPS'
+    else:
+        if len(args) == 3:
+            user = args[1]
+            group = args[2]
+        elif len(args) == 1:
+            # get os username by default
+            uinfo = pwd.getpwuid(os.getuid())
+            user = uinfo.pw_name
+            # group by default DATAOPS
+            group = 'DATAOPS'
+        else:
+            parser.error("Provide the workflow of a file of workflows")
+            sys.exit(1)
+        # name of workflow
+        wfs = [args[0]]
+
+    for wf in wfs:
+        workflow = reqMgrClient.Workflow(wf)
+        if workflow.status in {'assigned' , 'assignment-approved','completed','closed-out'}:
+            print("Rejecting workflow: " + wf)
+            reqMgrClient.rejectWorkflow(url, wf)
+        elif workflow.status in {'acquired','running-open','running-closed'}:
+            print("Aborting workflow: " + wf)
+            reqMgrClient.abortWorkflow(url, wf)
+        else:
+            print("The workflow '"+wf+ "' with status '"+workflow.status+"' cannot be rejected or aborted.")
+
+        # invalidates datasets
+        print "Invalidating datasets"
+        datasets = reqMgrClient.outputdatasetsWorkflow(url, wf)
+        for dataset in datasets:
+            print dataset
+            dbs3.setDatasetStatus(dataset, 'INVALID', files=True)
+
+        # clone workflow
+        if options.clone:
+            print("Clonning workflow: "+ wf)
+            cloned = resubmit.cloneWorkflow(wf, user, group)
+>>>>>>> Merging the rejecting scripts(rejectWorkflows.py,abortWorkflows.py,rejectAndClone.py and abortAndClone.p) and the resubmiting ones (resubmit.py and extendWorflow.py)
     sys.exit(0);
 
 if __name__ == "__main__":
