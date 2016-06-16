c03e89c97b239f1761713b48b68ff1bfe37c690c
Allison Reinsvold <areinsvo@nd.edu>
2016-06-16 13:50:03 -0400

    Add option to set memory in reject.py and resubmit.py, fix era/procstring in assign.py

--- reject.py
+++ reject.py
@@ -79,7 +79,7 @@ def main():
                 mem = options.memory
             else:
                 mem = workflowInfo.info["Memory"]
-                cloned = resubmit.cloneWorkflow(workflow, user, options.group, memory=mem)
+            cloned = resubmit.cloneWorkflow(workflow, user, options.group, memory=mem)
     sys.exit(0);
 
 if __name__ == "__main__":
