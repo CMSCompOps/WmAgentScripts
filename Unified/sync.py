56d16c4d188502b9a7e634e9b191dc1b9f19b195
jean-roch <vlimant@cern.ch>
2016-07-27 13:00:37 +0200

    change the command line options, and logging

--- Unified/sync.py
+++ Unified/sync.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 from assignSession import *
-from utils import getWorkLoad, reqmgr_url
+from utils import workflowInfo, reqmgr_url
 import sys
+import optparse
 
+parser = optparse.OptionParser()
+parser.add_option('-w',help='the workflow name to change the status of')
+parser.add_option('-s',help='the new status to be set')
+parser.add_option('--comments',help='an additional comment to register in the unified log',default=None)
+(options,args) = parser.parse_args()
 url = reqmgr_url
 
 if __name__ == "__main__":
-    spec = sys.argv[1]
-    status = None
-    if len(sys.argv)>2:
-        status = sys.argv[2]
-        
+    spec = options.w
+    status = options.s
+    comment = options.comments
+
+    if not status:
+        sys.exit(0)
+    if not spec:
+        sys.exit(0)
+
     for wf in session.query(Workflow).all():
         if spec and spec not in wf.name: continue
         #if not wf.status in ['away']: continue
 
-        wl = getWorkLoad(url, wf.name)
-        wf.wm_status = wl['RequestStatus']
+        old_status = wf.status 
+
+        wfi = workflowInfo(url, wf.name)
+        wf.wm_status = wfi.request['RequestStatus']
 
         if status:
             if status == 'DELETE':
@@ -40,4 +52,6 @@ if __name__ == "__main__":
         #elif wf.wm_status in ['assignment-approved']:
         #    wf.status = 'considered'
         print wf.name, wf.wm_status, wf.status
+
+        wfi.sendLog('sync','Setting status from %s to %s (%s)%s'%(old_status,wf.status,wf.wm_status,('\nreason: %s'%(comment) if comment else "")))
     session.commit()
