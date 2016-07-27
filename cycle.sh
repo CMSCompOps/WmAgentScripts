6600e2955f88fbadf5bbeda524d29791c4dd981b
jean-roch <vlimant@cern.ch>
2016-07-27 13:01:32 +0200

    subscribe dataops blocks

--- cycle.sh
+++ cycle.sh
@@ -21,7 +21,7 @@ source /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts/credentials.sh
 
 ## early assignement with all requirements included 
 /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts/cWrap.sh Unified/assignor.py --early --limit 100
-
+/afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts/cWrap.sh Unified/subscribor.py away
 
 ## verify sanity of completed workflow and pass along
 /afs/cern.ch/user/c/cmst2/Unified/WmAgentScripts/cWrap.sh Unified/checkor.py --strict
