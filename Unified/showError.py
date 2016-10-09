102d919e210b643a57104d036133500aa4ba5e7e
jean-roch <vlimant@cern.ch>
2016-10-09 18:24:16 +0200

    a lot more details in error reports

--- Unified/showError.py
+++ Unified/showError.py
@@ -1,23 +1,45 @@
-from utils import workflowInfo, siteInfo, monitor_dir, global_SI
+from utils import workflowInfo, siteInfo, monitor_dir, global_SI, getDatasetPresence
 import json
 import sys
 from collections import defaultdict
 from assignSession import *
 import time
+import optparse
 
-
-def parse_one(url, wfn):
+def parse_one(url, wfn, options=None):
 
     SI = global_SI()
     wfi = workflowInfo( url , wfn)
     where_to_run, missing_to_run,missing_to_run_at = wfi.getRecoveryInfo()       
+    
+    _,prim,_,sec = wfi.getIO()
 
-    cache = int(sys.argv[2]) if len(sys.argv)>2 else 0
+    cache = 0
+    if options:
+        cache = options.cache
     print "cache timeout", cache
 
     err= wfi.getWMErrors(cache=cache)
     stat = wfi.getWMStats(cache=cache)
-    #dashb = wfi.getFullPicture(since=30,cache=cache)
+    #adcd = wfi.getRecoveryDoc()
+
+    total_by_code_dash = defaultdict( int )
+    total_by_site_dash = defaultdict( int )
+    r_dashb =defaultdict( lambda : defaultdict( int ))
+    dash_board_h = 1
+    if True :#'pdmvserv_TOP-RunIISummer15wmLHEGS-00103_00183_v0__161005_165048_809' in wfn:
+        ## NB get the since from when the wf has started, not a fixed value
+        dashb = wfi.getFullPicture(since=dash_board_h,cache=cache)
+        #print json.dumps( dashb , indent=2)
+        for site,sinfo in dashb.items():
+            for s_code,counts in sinfo.items():
+                d_statuses = ['submitted','pending','app-unknown','done']
+                total_by_code_dash[str(s_code)]+= counts.get('submitted',0)
+                total_by_site_dash[site] += counts.get('submitted',0)
+                r_dashb[str(s_code)][site] += counts.get('submitted',0)
+
+        print json.dumps(total_by_code_dash , indent=2)
+        print json.dumps(total_by_site_dash , indent=2)
 
     status_per_task = defaultdict(lambda : defaultdict(int))
     for agent in stat['AgentJobInfo']:
@@ -38,7 +60,7 @@ def parse_one(url, wfn):
     #    for error in dashb[site]:
     #        db_total_per_site[site] += dashb[site][error] 
     #        db_total_per_code[code] += dashb[site][error]
-
+    
     print "will be willing to run at"      
     print json.dumps( where_to_run , indent=2)         
     print json.dumps(missing_to_run , indent=2)        
@@ -47,31 +69,70 @@ def parse_one(url, wfn):
     task_error_site_count ={}
     one_explanation = defaultdict(set)
 
-    expose_archive_code = {'134':defaultdict(lambda : 10),#seg fault
-                           '99109':defaultdict(lambda : 10),#stageout
-                           '99303' : defaultdict(lambda : 10),#no pkl report. if you are lucky
-                           '60450' : defaultdict(lambda : 10),#new
-                           '50513':defaultdict(lambda : 10),#new
-                           '8001': defaultdict(lambda : 10),# the usual exception in cmsRun
+    do_JL = True
+    do_CL = True
+    if options: 
+        do_JL = not options.no_JL
+        do_CL = not options.no_CL
+        
+    n_expose = 1
+    expose_archive_code = {'134':defaultdict(lambda : n_expose),#seg fault
+                           '139':defaultdict(lambda : n_expose),# ???
+                           '99109':defaultdict(lambda : n_expose),#stageout
+                           '99303' : defaultdict(lambda : n_expose),#no pkl report. if you are lucky
+                           '60450' : defaultdict(lambda : n_expose),#new
+                           '50513':defaultdict(lambda : n_expose),#new
+                           '8001': defaultdict(lambda : n_expose),# the usual exception in cmsRun
+                           '11003': defaultdict(lambda : n_expose),# job extraction
                            }
-    expose_condor_code = {'99109':defaultdict(lambda : 10),#stageout
-                          '99303':defaultdict(lambda : 10),#no pkl report
-                          '60450':defaultdict(lambda : 10),#new
-                          '50513':defaultdict(lambda : 50),#new
+    expose_condor_code = {'99109':defaultdict(lambda : n_expose),#stageout
+                          '99303':defaultdict(lambda : n_expose),#no pkl report
+                          '60450':defaultdict(lambda : n_expose),#new
+                          '50513':defaultdict(lambda : n_expose),#new
+                          '11003': defaultdict(lambda : n_expose),
                           }
-    tasks = err.keys()
-    tasks.sort()
+    
+    tasks = sorted(set(err.keys() + missing_to_run.keys()))
+
     if not tasks:
         return task_error_site_count
         
-    html="<html> <center><h1>%s<br><a href=https://dmytro.web.cern.ch/dmytro/cmsprodmon/workflows.php?prep_id=%s>%s</a></center> <br>Updated on %s (GMT)<hr><br>"%(
+    html="<html> <center><h1>%s<br><a href=https://dmytro.web.cern.ch/dmytro/cmsprodmon/workflows.php?prep_id=%s>%s</a><br>"%(
         tasks[0].split('/')[1],
         wfi.request['PrepID'],
-        wfi.request['PrepID'],
-        time.asctime(time.gmtime())
+        wfi.request['PrepID']
         )
     if wfi.request['RequestType'] in ['ReReco']:
-        html += '<a href=../datalumi/lumi.%s.html>lumis</a><br>'% wfi.request['PrepID']
+        html += '<a href=../datalumi/lumi.%s.html>Lumisection Summary</a><br>'% wfi.request['PrepID']
+        
+    html+= '</center><hr>'
+    if prim:
+        html+='Reads in primary<br>'
+        for dataset in prim:
+            presence = getDatasetPresence(url, dataset)
+            html +='<b>%s</b><ul>'%dataset
+            for site in sorted(presence.keys()):
+                html += '<li>%s : %.2f %%'%( site, presence[site][1] )
+            html+='</ul><br>'
+    if sec:
+        html+='Reads in secondary<br>'
+        for dataset in sec:
+            presence = getDatasetPresence(url, dataset)
+            html +='<b>%s</b><ul>'%dataset
+            for site in sorted(presence.keys()):
+                html += '<li>%s : %.2f %%'%( site, presence[site][1] )
+            html+='</ul>'
+        
+    html += "Updated on %s (GMT)" % ( time.asctime(time.gmtime()) )
+    html += """
+<ul>
+<li> <b><i>dashboard numbers over %d days</b></i>
+<li> &uarr; %% with respect to total number of error in the code
+<li> &rarr; %% with respect to total number of error at the site
+</ul>
+"""%(dash_board_h)
+
+    html += '<hr><br>'
     for task in tasks:  
         print task
         total_per_site = defaultdict(int)
@@ -98,7 +159,11 @@ def parse_one(url, wfn):
 
         total_count= defaultdict(int)
         error_site_count = defaultdict( lambda : defaultdict(int))
+        if not task in err:
+            print task,"has not reported error"
+            err[task] = {}
         print err[task].keys()
+        
         for exittype in err[task]:
             #print "\t",err[task][exittype].keys()
             for errorcode_s in err[task][exittype]:
@@ -123,14 +188,14 @@ def parse_one(url, wfn):
                         wmbs = sample['wmbsid']
                         workflow = sample['workflow']
 
-                        if errorcode_s in expose_condor_code and expose_condor_code[errorcode_s][agent]:
+                        if do_CL and errorcode_s in expose_condor_code and expose_condor_code[errorcode_s][agent]:
                             os.system('ssh %s /afs/cern.ch/user/v/vlimant/scratch0/ops/central_ops/WmAgentScripts/bumbo.sh %s %s %s'%( agent, workflow, wmbs, errorcode_s))
                             expose_condor_code[errorcode_s][agent]-=1
 
                         for out in sample['output']:
                             #print out
                             if out['type'] == 'logArchive':
-                                if errorcode_s in expose_archive_code and expose_archive_code[errorcode_s][agent]:
+                                if do_JL and errorcode_s in expose_archive_code and expose_archive_code[errorcode_s][agent]:
                                     expose_archive_code[errorcode_s][agent]-=1
                                     os.system('mkdir -p /tmp/%s'%(os.getenv('USER')))
                                     local = '/tmp/%s/%s'%(os.getenv('USER'),out['lfn'].split('/')[-1])
@@ -152,7 +217,7 @@ def parse_one(url, wfn):
                                             trunc = '/tmp/%s/%s'%(os.getenv('USER'), label)
                                             #print fn
                                             #print trunc
-                                            head = tail = 400
+                                            head = tail = 1000
                                             os.system('(head -%d ; echo;echo;echo "<snip>";echo;echo ; tail -%d ) < %s > %s'%(head, tail, fn, trunc))
                                             os.system('mv %s %s'%(trunc, fn))
 
@@ -170,9 +235,25 @@ def parse_one(url, wfn):
         #success = total_count['0']
         #total_jobs = sum(total_count.values())
         #print total_jobs,"jobs in total,",success,"successes"
-        miss = "{:,}".format(missing_to_run[task]) if task in missing_to_run else "N/A"
-        html += "<b>%s</b> is missing <b>%s events</b>"%(task.split('/')[-1], miss)
+        #miss = "{:,}".format(missing_to_run[task]) if task in missing_to_run else "N/A"
+
+        ## show the total
+        s_per_code =defaultdict(int)
+        for site in all_sites:
+            for code in sorted(all_codes):
+                s_per_code[code] += error_site_count[code][site]
+
+        no_error = (sum(s_per_code.values())==0)
+        missing_events = missing_to_run[task] if task in missing_to_run else 0
+        html += "<b>%s</b>"%task.split('/')[-1]
+        if missing_events:
+            html += " is missing <b>%s events</b>"%( missing_events )
+            if no_error:
+                html +="<br><b><font color=red> and has no reported error</font></b>"
+
+
         html += "<br><table border=1><thead><tr><th>Sites/Errors</th>"
+
         #for site in all_sites:
         #    html+='<th>%s</th>'%site
         for code in sorted(all_codes):
@@ -185,14 +266,12 @@ def parse_one(url, wfn):
 
         html+='<th>Total jobs</th><th>Site Ready</th>'
         html+='</tr></thead>\n'
-        ## show the total
-        s_per_code =defaultdict(int)
-        for site in all_sites:
-            for code in sorted(all_codes):
-                s_per_code[code] += error_site_count[code][site]
+
         html+='<tr><td>Total</td>'
         for code in sorted(all_codes):
             html += '<td bgcolor=orange width=100>%d'%(s_per_code[code])
+            if code in total_by_code_dash:
+                html += ' (<b><i>%d</i></b>)'% total_by_code_dash[code]
             html += '</td>'
 
         ulist='<ul>'
@@ -221,7 +300,7 @@ def parse_one(url, wfn):
             there = max([k for k in _range.keys() if k<=frac])
             return _range[there]
 
-        for site in all_sites:
+        for site in sorted(all_sites):
             site_in = 'Yes' if site in SI.sites_ready else 'No'
             color = 'bgcolor=pink' if not site in SI.sites_ready else 'bgcolor=lightblue'
             html+='<tr><td %s>%s</td>'%(color,site)
@@ -229,11 +308,14 @@ def parse_one(url, wfn):
                 if error_site_count[code][site]:
                     er_frac = float(error_site_count[code][site])/s_per_code[code] if s_per_code[code] else 0.
                     si_frac = float(error_site_count[code][site])/total_per_site[site] if total_per_site[site] else 0.
-                    html += '<td %s width=200>%d, <font color=%s>&uarr; %.1f%%</font>, <font color=%s>&rarr; %.1f%%</font></td>'% (color,
-                                                                                                                             error_site_count[code][site],
-                                                                                                                             palette(er_frac),100.*er_frac,
-                                                                                                                             palette(si_frac), 100.*si_frac
-                                                                                                                             )
+                    html += '<td %s width=200>%d'%(color, error_site_count[code][site])
+                    if code in r_dashb and site in r_dashb[code]:
+                        html += ' (<b><i>%d</i></b>)'%( r_dashb[code][site] )
+
+                    html += ', <font color=%s>&uarr; %.1f%%</font>, <font color=%s>&rarr; %.1f%%</font></td>'% (
+                        palette(er_frac),100.*er_frac,
+                        palette(si_frac), 100.*si_frac
+                        )
 
 
                 else:
@@ -258,11 +340,11 @@ def parse_one(url, wfn):
 
     return task_error_site_count, one_explanation
 
-def parse_all(url):
+def parse_all(url, options=None):
     explanations = defaultdict(set)
     alls={}
     for wfo in session.query(Workflow).filter(Workflow.status == 'assistance-manual').all():    
-        task_error, one_explanation = parse_one( url, wfo.name)
+        task_error, one_explanation = parse_one( url, wfo.name, options)
         alls.update( task_error )
         for code in one_explanation:
             explanations[code].update( one_explanation[code] )
@@ -289,7 +371,14 @@ def parse_all(url):
 if __name__=="__main__":
     url = 'cmsweb.cern.ch'
 
-    if len(sys.argv)>1:
-        parse_one(url, sys.argv[1])
+    parser = optparse.OptionParser()
+    parser.add_option('--no_JL',help="Do not get the job logs", action="store_true",default=False)
+    parser.add_option('--no_CL',help="Do not get the condor logs", action="store_true",default=False)
+    parser.add_option('--cache',help="The age in second of the error report before reloading them", default=0, type=float)
+    parser.add_option('--workflow','-w',help="The workflow to make the error report of",default=None)
+    (options,args) = parser.parse_args()
+    
+    if options.workflow:
+        parse_one(url, options.workflow, options)
     else:
-        parse_all(url)
+        parse_all(url, options)
