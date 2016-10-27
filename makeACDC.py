f3b948df09aabdee593482d405c6ce9773ca2a27
jean-roch <vlimant@cern.ch>
2016-10-27 11:48:31 +0200

    coma separated list of task to recover

--- makeACDC.py
+++ makeACDC.py
@@ -35,14 +35,14 @@ def makeACDC(url, wfi, task, memory=None):
 def main():
 
     #Create option parser
-    usage = "usage: %prog [options] [WORKFLOW] TASK"
+    usage = "usage: %prog (-w workflow|-f filelist) (-t TASK|--all) [--tesbed]"
     parser = OptionParser(usage=usage)
     parser.add_option("-f","--file", dest="file", default=None,
                         help="Text file with a list of workflows")
     parser.add_option("-w","--workflow", default=None,
                       help="Coma separated list of wf to handle")
-    parser.add_option("-t","--task",
-                      help="The task to be recovered")
+    parser.add_option("-t","--task", default=None,
+                      help="Coma separated task to be recovered")
     parser.add_option("-a","--all",
                       help="Make acdc for all tasks to be recovered",default=False, action='store_true')
     parser.add_option("-m","--memory", dest="memory", default=None, type=float,
@@ -60,11 +60,11 @@ def main():
     elif options.workflow:
         wfs = options.workflow.split(',')
     else:
-        print "Either provide a -f filelist or a -w workflow"
+        parser.error("Either provide a -f filelist or a -w workflow")
         sys.exit(1)
 
-    if not wfs and not options.task:
-        parser.error("Provide the Workflow Name and the Task Name")
+    if (not wfs) or (not options.task and not options.all):
+        parser.error("Provide the -w Workflow Name and the -t Task Name or --all")
         sys.exit(1)
 
     for wfname in wfs:
@@ -72,28 +72,24 @@ def main():
         if options.task == 'all' or options.all:
             where,how_much,how_much_where = wfi.getRecoveryInfo()
             tasks = sorted(how_much.keys())
-            created = {}
-            print tasks
-            for task in tasks:
-                r = makeACDC(url, wfi, task, options.memory) 
-                if not r: 
-                    print "Error in creating ACDC for",task,"on",wfname
-                    break
-                created[task] = r
-            if len(created)!=len(tasks):
-                print "Error in creating all required ACDCs"
-                sys.exit(1)
-            print "Created:"
-            for task in created:
-                print created[task],"for",task
-
         else:
-            r = makeACDC(url, wfi, options.task, options.memory)
-            if not r:
-                print "Error in creating ACDC"
-                sys.exit(1)
-            print "Created:" 
-            print r
+            tasks = options.task.split(',')
+
+        created = {}
+        print "Workflow:",wfname
+        print "Tasks:",tasks
+        for task in tasks:
+            r = makeACDC(url, wfi, task, options.memory) 
+            if not r: 
+                print "Error in creating ACDC for",task,"on",wfname
+                break
+            created[task] = r
+        if len(created)!=len(tasks):
+            print "Error in creating all required ACDCs"
+            sys.exit(1)
+        print "Created:"
+        for task in created:
+            print created[task],"for",task
 
 if __name__ == '__main__':
     main()
