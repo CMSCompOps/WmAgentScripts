37dd4204facaee3997d9956aa13f6bea0d7ec214
jean-roch <vlimant@cern.ch>
2016-11-25 09:53:24 +0100

    clean the afs cache

--- cleanlog.sh
+++ cleanlog.sh
@@ -1,6 +1,6 @@
 for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.log` ; do
     age=$((($(date +%s)-$(date +%s -r $each))/86400))
-    if [ $age -gt 40 ] ; then
+    if [ $age -gt 10 ] ; then
 	echo remove $each
 	rm -f $each
     fi
@@ -13,6 +13,14 @@ for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.time` ; do
     fi
 done
 for each in `ls /afs/cern.ch/user/c/cmst2/www/unified/logs/*/*.json` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each  maybe
+	#rm -f $each
+    fi
+done
+
+for each in `ls /afs/cern.ch/work/c/cmst2/unified/cache/*` ; do
     age=$((($(date +%s)-$(date +%s -r $each))/86400))
     if [ $age -gt 10 ] ; then
 	echo remove $each  maybe
