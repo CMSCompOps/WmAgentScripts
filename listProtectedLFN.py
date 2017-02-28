1f868075f1ead622b062bbeebf044c17dae29032
jean-roch <vlimant@cern.ch>
2017-02-28 19:17:10 +0100

    add also lfn of intermediate outputs

--- listProtectedLFN.py
+++ listProtectedLFN.py
@@ -4,12 +4,13 @@ import os
 import json
 import time
 from collections import defaultdict
-from utils import monitor_pub_dir
+from utils import monitor_pub_dir, workflowInfo
 
 register=['assigned','acquired','running-open','running-closed','force-complete','completed','closed-out']
 wfs = []
+url = 'cmsweb.cern.ch'
+
 def getWorkflows( status ):
-    url = 'cmsweb.cern.ch'
     conn  =  httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
     go_to = '/reqmgr2/data/request?status=%s&detail=true'%status
     r1=conn.request("GET",go_to, headers={"Accept":"application/json"})        
@@ -25,25 +26,40 @@ for r in register:
     wfs.extend( getWorkflows( r ) )
     print len(wfs),"after collecting",r
 
+from WMCore.WMSpec.WMWorkload import WMWorkloadHelper
 lfns =defaultdict(set)
 for wf in wfs:
-    outs = wf['OutputDatasets']
-    base=wf.get('UnmergedLFNBase','/store/unmerged')
-    for out in outs:
-        _,dsn,ps,tier = out.split('/')
-        acq,rest = ps.split('-',1)
-        print wf['RequestName'],wf['RequestStatus']
-        d = '/'.join( [ base, acq, dsn, tier, rest] )
-        print d
-        lfns[d].add( wf['RequestName'] )
+    wfi = workflowInfo( url , wf['RequestName'] , request=wf)
 
+    help = WMWorkloadHelper( wfi.full_spec )
+
+    for task in help.getAllTasks():
+        for step in task.listAllStepNames():
+            o = task.getOutputModulesForStep( step )
+            for name,module in o.dictionary_().items():
+                if hasattr(module, "lfnBase"):
+                    base = getattr(module,"lfnBase")
+                    if 'unmerged' in base:
+                        lfns[base].add( wf['RequestName'] )
+    continue                    
+    #outs = wf['OutputDatasets']
+    #base=wf.get('UnmergedLFNBase','/store/unmerged')
+    #for out in outs:
+    #    _,dsn,ps,tier = out.split('/')
+    #    acq,rest = ps.split('-',1)
+    #    print wf['RequestName'],wf['RequestStatus']
+    #    d = '/'.join( [ base, acq, dsn, tier, rest] )
+    #    print d
+    #    lfns[d].add( wf['RequestName'] )
+        
 now = time.gmtime()
 content = { "timestamp" : time.mktime(now),
             "date" : time.asctime(now),
             "protected" : sorted(lfns.keys())
             }
-#open('listProtectedLFN.txt','w').write( json.dumps( sorted(lfns.keys()), indent=2))    
+
 open('%s/listProtectedLFN.txt'%monitor_pub_dir,'w').write( json.dumps( content, indent=2))    
+#open('%s/listProtectedLFN.txt.2'%monitor_pub_dir,'w').write( json.dumps( content, indent=2))    
 #print '\n'.join( sorted(lfns.keys()) )
 
 for lfn in lfns:
