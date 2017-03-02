15ec431e5e15c318a44d9bc0b7409c6429d0563f
jean-roch <vlimant@cern.ch>
2017-03-02 12:38:13 +0100

    new location

--- Unified/my_condor.py
+++ Unified/my_condor.py
@@ -1,6 +1,6 @@
 import json
-from utils import monitor_dir
+from utils import monitor_pub_dir
 from go_condor import makeAds
 
-config = json.loads(open('%s/equalizor.json'%monitor_dir).read())  
+config = json.loads(open('%s/equalizor.json'%monitor_pub_dir).read())  
 makeAds( config )
