ce9961447c409f029d973a245da72a07d411ec1e
jean-roch <vlimant@cern.ch>
2017-10-16 08:54:02 +0200

    with options

--- acdc.py
+++ acdc.py
@@ -2,7 +2,16 @@
 import sys
 import os
 
-wfs=sys.argv[1].split(',')
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option('--go',default=False, action = 'store_true', help='Answer yes to all')
+parser.add_option('--workflows',help='Coma separated list of workflows to recover')
+def_assignoptions = '--site acdc --xrootd'
+parser.add_option('--assignoptions',default=def_assignoptions, help='The options to pass to assign.py (default is %s)'%def_assignoptions)
+(options,args) = parser.parse_args()
+
+wfs=options.workflows.split(',')
 
 for wf in wfs:
     makeall = os.popen('./makeACDC.py --all -w %s'% wf).read()
@@ -13,7 +22,8 @@ for wf in wfs:
         if s[1]!='for' : continue
         acdcs.append( s[0] )
     print acdcs
-    sec='--secondary_x' if 'RunIISummer16DR80Premix' in wf else ''
+    #sec='--secondary_x' if 'RunIISummer16DR80Premix' in wf else ''
+    sec=''
     for acdc in acdcs:
-        os.system('./assign.py -s acdc -w %s %s'%(acdc, sec))
+        os.system('./assign.py %s -w %s %s'%(options.assignoptions, acdc, sec))
         pass
