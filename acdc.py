57e467085090249aa52d8c701a07c1137ea575d3
jean-roch <vlimant@cern.ch>
2017-10-18 12:51:17 +0200

    some skip options

--- acdc.py
+++ acdc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
-
+from utils import getWorkflowById, reqmgr_url, workflowInfo
 import optparse
 
 parser = optparse.OptionParser()
@@ -9,18 +9,26 @@ parser.add_option('--go',default=False, action = 'store_true', help='Answer yes 
 parser.add_option('--workflows',help='Coma separated list of workflows to recover')
 def_assignoptions = '--site acdc --xrootd'
 parser.add_option('--assignoptions',default=def_assignoptions, help='The options to pass to assign.py (default is %s)'%def_assignoptions)
+parser.add_option('--nocreation', default=False, action = 'store_true', help='Do not inject new request')
 (options,args) = parser.parse_args()
 
 wfs=options.workflows.split(',')
 
 for wf in wfs:
-    makeall = os.popen('./makeACDC.py --all -w %s'% wf).read()
     acdcs = []
-    for s in [l.split() for l in makeall.split('\n')]:
-        #print s
-        if len(s)!=3: continue
-        if s[1]!='for' : continue
-        acdcs.append( s[0] )
+
+    if options.nocreation:
+        wfi = workflowInfo( reqmgr_url, wf)
+        pid = wfi.request['PrepID']
+        familly = filter(lambda r : r['RequestStatus'] == 'assignment-approved' and 'ACDC' in r['RequestName'],
+                         getWorkflowById( reqmgr_url, pid, details=True))
+        acdcs = [r['RequestName'] for r in familly]
+    else:
+        makeall = os.popen('./makeACDC.py --all -w %s'% wf).read()
+        for s in [l.split() for l in makeall.split('\n')]:
+            if len(s)!=3: continue
+            if s[1]!='for' : continue
+            acdcs.append( s[0] )
     print acdcs
     #sec='--secondary_x' if 'RunIISummer16DR80Premix' in wf else ''
     sec=''
