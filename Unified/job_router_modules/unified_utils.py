d5258e82f38da6797b0eab57b5e0d6095506ff5c
jean-roch <vlimant@cern.ch>
2018-02-06 11:10:14 +0100

    prevent from inserting in DESIRED_Sites sites that are not ready

--- Unified/job_router_modules/unified_utils.py
+++ Unified/job_router_modules/unified_utils.py
@@ -20,10 +20,16 @@ def siteMapping(in_list, source_to_dests, state={}):
     if isinstance(in_list, classad.Value):
         return classad.Value.Undefined
 
+    possible_sources = source_to_dests.keys()
     split_list = _split_re.split(in_list)
     final_set = set()
     for site in split_list:
+        ## remove the sites that are not in the mapping : i.e. down/drain and whatnot
+        if not site in possible_sources: continue
+
+        ## add the source sites
         final_set.add(site)
+        ## add all destination sites
         final_set.update(source_to_dests.setdefault(site, set()))
     split_list = list(final_set)
     split_list.sort()
