286f194649e1df56189067d28dc6a5b002207916
jean-roch <vlimant@cern.ch>
2018-02-20 20:08:32 +0100

    clean eos and local

--- cleanlog.sh
+++ cleanlog.sh
@@ -1,4 +1,5 @@
-HTML_DIR=/var/www/html/unified/
+HTML_DIR=/eos/project/c/cms-unified-logs/www/
+LOCAL_DIR=/data/unified/www/
 CACHE_DIR=/data/unified-cache/
 
 for each in `ls $HTML_DIR/logs/*/*.log` ; do
@@ -16,6 +17,28 @@ for each in `ls $HTML_DIR/logs/*/*.time` ; do
     fi
 done
 for each in `ls $HTML_DIR/logs/*/*.json` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each
+	rm -f $each
+    fi
+done
+
+for each in `ls $LOCAL_DIR/logs/*/*.log` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each
+	rm -f $each
+    fi
+done
+for each in `ls $LOCAL_DIR/logs/*/*.time` ; do
+    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+    if [ $age -gt 10 ] ; then
+	echo remove $each
+	rm -f $each
+    fi
+done
+for each in `ls $LOCAL_DIR/logs/*/*.json` ; do
     age=$((($(date +%s)-$(date +%s -r $each))/86400))
     if [ $age -gt 10 ] ; then
 	echo remove $each
