d75c80a2da777b9d7230019a9d3a27858dc6f866
jean-roch <vlimant@cern.ch>
2018-02-26 11:16:33 +0100

    only env var by default. creating in crontab

--- credentials.sh
+++ credentials.sh
@@ -1,10 +1,13 @@
+
 #talk to mcm
 export MCM_SSO_COOKIE=/tmp/$USER-$HOSTNAME-vfsvdka573t
-echo "creating mcm cookie" $MCM_SSO_COOKIE
-cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o $MCM_SSO_COOKIE --krb
+if [ "$1" == "create" ] ; then
+    echo "creating mcm cookie" $MCM_SSO_COOKIE
+    cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o $MCM_SSO_COOKIE --krb
+fi
 
 #talk to cmsweb
-#export X509_USER_PROXY=$HOME/private/personal/voms_proxy.cert
 export X509_USER_PROXY=/tmp/$USER-4hty64k793hj
-cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
-
+if [ "$1" == "create" ] ; then
+    cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
+fi
