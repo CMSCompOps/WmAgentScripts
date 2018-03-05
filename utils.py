4d7849fd88d8135a0d4bfe180bd2456e68633a17
jean-roch <vlimant@cern.ch>
2018-03-05 20:53:56 +0100

    another splitting parameter to drop

--- utils.py
+++ utils.py
@@ -6348,7 +6348,7 @@ class workflowInfo:
         for spl in result:
             if not spl['taskType'] in ['Production','Processing','Skim'] : continue
             if strip:
-                for drop in ['algorithm','trustPUSitelists','trustSitelists','deterministicPileup','type','include_parents','lheInputFiles','runWhitelist','runBlacklist']:
+                for drop in ['algorithm','trustPUSitelists','trustSitelists','deterministicPileup','type','include_parents','lheInputFiles','runWhitelist','runBlacklist','collectionName']:
                     if drop in spl['splitParams']:
                         spl['splitParams'].pop(drop)
             splittings.append( spl )
