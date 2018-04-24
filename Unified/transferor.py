ecae7c05631ddfe23baca4c4d4b2c748957e3b92
jean-roch <vlimant@cern.ch>
2018-04-24 16:31:21 +0200

    remove this limitation when reaching transfer budget

--- Unified/transferor.py
+++ Unified/transferor.py
@@ -339,9 +339,8 @@ def transferor(url ,specific = None, talk=True, options=None):
                         no_budget = True
 
 
-        if no_budget:
-            break ## try this for a while to make things faster
-            #continue
+        #if no_budget:
+        #    break ## try this for a while to make things faster
 
         ## the site white list considers site, campaign, memory and core information
         if options and options.tosites:
