00c04d75854dc196d049b540b249f6eb398c5307
jean-roch <vlimant@cern.ch>
2018-05-07 11:15:37 +0200

    HN from CERN

--- Unified/equalizor.py
+++ Unified/equalizor.py
@@ -122,12 +122,8 @@ def equalizor(url , specific = None, options=None):
     mapping['T1_DE_KIT'].append( 'T2_CH_CERN' )
     mapping['T2_CH_CERN'].append( 'T1_IT_CNAF' )
     mapping['T2_CH_CERN'].append( 'T1_US_FNAL' )
-    #mapping['T2_CH_CERN'].append( 'T3_IN_TIFRCloud' )
-    #mapping['T1_IT_CNAF'].append( 'T3_IN_TIFRCloud' )
-    #mapping['T2_UK_London_IC'].append( 'T2_CH_CERN' )
-    #mapping['T1_UK_RAL'].append( 'T2_BE_IIHE' )
-    #mapping['T2_UK_London_IC'].append( 'T2_BE_IIHE' )
-    #mapping['T2_UK_London_IC'].append( 'T2_FR_CCIN2P3' )
+    mapping['T2_CH_CERN'].append('T3_CH_CERN_HelixNebula')
+
     for site in sites_to_consider:
         if '_US_' in site:
             mapping[site].append('T2_CH_CERN')
