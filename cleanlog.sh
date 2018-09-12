4e0749bc7a7a939f60039aa7f0bb6ea790213c27
jean-roch <vlimant@cern.ch>
2018-09-12 14:55:26 +0200

    no need to clean .time files

--- cleanlog.sh
+++ cleanlog.sh
@@ -12,15 +12,15 @@ for each in `find $HTML_DIR/logs/ -name *.log` ; do
 	rm -f $each
     fi
 done
-echo "cleaning .time files in html dir"
-#for each in `ls $HTML_DIR/logs/*/*.time` ; do
-for each in `find $HTML_DIR/logs/ -name *.time` ; do
-    age=$((($(date +%s)-$(date +%s -r $each))/86400))
-    if [ $age -gt 15 ] ; then
-	echo remove $each
-	rm -f $each
-    fi
-done
+#echo "cleaning .time files in html dir"
+##for each in `ls $HTML_DIR/logs/*/*.time` ; do
+#for each in `find $HTML_DIR/logs/ -name *.time` ; do
+#    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+#    if [ $age -gt 15 ] ; then
+#	echo remove $each
+#	rm -f $each
+#    fi
+#done
 echo "cleaning .json files in html dir"
 #for each in `ls $HTML_DIR/logs/*/*.json` ; do
 for each in `find $HTML_DIR/logs/ -name *.json` ; do
@@ -39,15 +39,15 @@ for each in `find $LOCAL_DIR/logs/ -name *.log` ; do
 	rm -f $each
     fi
 done
-echo "cleaning .time files in local dir"
-#for each in `ls $LOCAL_DIR/logs/*/*.time` ; do
-for each in `find $LOCAL_DIR/logs/ -name *.time` ; do
-    age=$((($(date +%s)-$(date +%s -r $each))/86400))
-    if [ $age -gt 15 ] ; then
-	echo remove $each
-	rm -f $each
-    fi
-done
+#echo "cleaning .time files in local dir"
+##for each in `ls $LOCAL_DIR/logs/*/*.time` ; do
+#for each in `find $LOCAL_DIR/logs/ -name *.time` ; do
+#    age=$((($(date +%s)-$(date +%s -r $each))/86400))
+#    if [ $age -gt 15 ] ; then
+#	echo remove $each
+#	rm -f $each
+#    fi
+#done
 #for each in `ls $LOCAL_DIR/logs/*/*.json` ; do
 for each in `find $LOCAL_DIR/logs/ -name *.json` ; do
     age=$((($(date +%s)-$(date +%s -r $each))/86400))
