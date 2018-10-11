018509fd6c5d1063f1f7270bc3a281b50ae352e7
jean-roch <vlimant@cern.ch>
2018-10-11 11:00:25 +0200

    nudging eos to keep it alive

---
+++ pokeeos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+out=/data/unified-cache/listingeos.$$.log
+date > $out
+ls /eos/cms/store/unified/ >> $out
+date >> $out
+echo >> $out
+ls /eos/cms/store/unified/www/ >>$out
+date >> $out
+echo >> $out
+ls /eos/cms/store/logs/prod/2018/* -l >>$out
+date >> $out
+
+cp $out /eos/cms/store/unified/www/eospoke/.
+ls /eos/cms/store/unified/www/eospoke/*.log
