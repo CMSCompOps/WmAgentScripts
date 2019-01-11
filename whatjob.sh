66edd084a8c80ea4636eb2ff212df6d923d27d40
jean-roch <vlimant@cern.ch>
2019-01-11 10:25:26 +0100

    clean

--- whatjob.sh
+++ whatjob.sh
@@ -1,22 +1,18 @@
-date
-#echo "################ LOCKS #######################"
-#ls -trlh /eos/cms/store/unified/*.lock
-#ls -trlh /eos/cms/store/unified/www/*.lock
-#ls -trlh /eos/cms/store/unified/www/public/*.lock
+date 
 
 echo "################ QUOTA #######################"
 eos quota ls -u cmsvoc /eos/cms/store/unified
 
 for each in 0268 0269 0272 0273 0274 0275; do
-    echo 
-    echo "#############################################"
-    echo "Set on node" $each
+    echo
+    echo "#############################################" 
+    echo "Set on node" $each 
     echo "################ CRON #######################"
-    acrontab -l | grep $each
+    acrontab -l | grep $each 
     echo "############## UPTIME #######################"
     ssh  vocms$each "uptime"
     echo "############## RUNNING ######################"
-    ssh  vocms$each "ps -e -f | grep Uni| grep -v grep" ## pulls in creationg of cookie and proxy
+    ssh  vocms$each "ps -e -f | grep Uni| grep -v grep"
 
     ssh vocms$each python /data/unified/WmAgentScripts/Unified/deadlock.py
 done
