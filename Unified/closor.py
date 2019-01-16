0fa4ea28f354f7a1942e8b49cb5909df161f5718
jean-roch <vlimant@cern.ch>
2019-01-16 16:31:13 +0100

    real timing per thread

--- Unified/closor.py
+++ Unified/closor.py
@@ -198,6 +198,7 @@ def closor(url, specific=None, options=None):
     closers = []
 
     print len(wfs),"closing"
+    run_threads.start()
 
     for iwfo,wfo in enumerate(wfs):
         if specific and not specific in wfo.name: continue
@@ -224,7 +225,7 @@ def closor(url, specific=None, options=None):
                                  verbose = True,
                                  label = 'closor')
     th_start = time.mktime(time.gmtime())
-    run_threads.start()
+
 
     ## waiting on all to complete
     while run_threads.is_alive():
