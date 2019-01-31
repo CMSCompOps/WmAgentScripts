e953b9273bdb7a63b05e9b993d29667e45f5d59a
jean-roch <vlimant@cern.ch>
2019-01-31 16:49:43 +0100

    add the hold/bypass/force mongodb interface

---
+++ wtcActions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from utils import wtcInfo
+import optparse
+import os
+user = os.environ.get('USER')
+
+parser = optparse.OptionParser()
+parser.add_option('--action', choices=['hold','bypass','force'])
+parser.add_option('--keyword')
+parser.add_option('--pop',action='store_true')
+(options,args) = parser.parse_args()
+
+WI = wtcInfo()
+if options.pop:
+    WI.remove( options.keyword )
+else:
+    WI.add( action= options.action,
+            keyword = options.keyword,
+            user= user)
+
