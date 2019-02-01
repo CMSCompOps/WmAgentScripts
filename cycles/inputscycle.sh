af4448613e2eea1d0953c2ab7f2c8f7bcc5281c0
jean-roch <vlimant@cern.ch>
2019-02-01 13:08:30 +0100

    centralize the cycles

---
+++ cycles/inputscycle.sh
@@ -0,0 +1,16 @@
+cd /data/unified/WmAgentScripts
+
+oweek=`date +%W`
+week=${oweek#0}
+let oddity=week%2
+
+if [ "$USER" != "vlimant" ] ; then
+    echo "single user running from now on"
+    exit
+fi
+
+export X509_USER_PROXY=$HOME/private/personal/voms_proxy.cert
+source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
+
+python Unified/inputs_summary.py 200
+python Unified/inputs_summary.py pertape
