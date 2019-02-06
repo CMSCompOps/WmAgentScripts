bcd1f561641685104d730205b42ec1f0889552ea
jean-roch <vlimant@cern.ch>
2019-02-06 12:43:22 +0100

    what if no comments

--- Unified/htmlor.py
+++ Unified/htmlor.py
@@ -1438,7 +1438,7 @@ remaining_bar_%s.draw(data_remain_%s, {title: '%s [TB]'});
                             j = jiras[0]
                             ## add a comment to that JIRA : experimental
                             last_comment_time = time.mktime(time.strptime(j.fields.comment.comments[-1].updated.split('.')[0],
-                                                              "%Y-%m-%dT%H:%M:%S"))
+                                                              "%Y-%m-%dT%H:%M:%S")) if hasattr(j.fields, 'comment') else now
                             ## 4h at least between pings in the agent comment
                             if (last_comment_time - now) > (agent_comment_graceperiod*60*60):
                                 JC.comment(j.key, alert_summary)
