87e005fb422ba7b26cd56605eaa3384fe92b10cf
jean-roch <vlimant@cern.ch>
2019-02-27 11:46:20 +0100

    simple rules

--- Unified/rulor.py
+++ Unified/rulor.py
@@ -1,15 +1,57 @@
 #!/usr/bin/env python
 from collections import deque
 from assignSession import *
-from utils import closeoutInfo, reportInfo, reqmgr_url, componentInfo, moduleLock
+from utils import closeoutInfo, reportInfo, reqmgr_url, componentInfo, moduleLock, workflowInfo
+from collections import defaultdict
 from wtcClient import wtcClient
 from JIRAClient import JIRAClient
 import optparse
 import json
 
+def sum_per_code( task_dict ):
+    s = defaultdict(int)
+    for code,codes in task_dict.items():
+        for site,count in codes.items():
+            s[code] += count
+    return dict(s)
 
+def main_errors( task_dict ):
+    per_code = sorted(sum_per_code(task_dict).items(), key = lambda o:o[1])
+    return per_code
+
+def dominant_error(task_dict, fraction = 0.5 ):
+    per_code = main_errors( task_dict )
+    s = sum([p[1] for p in per_code])
+    return (float(per_code[-1][1]) / float(s) > fraction, per_code[-1][0])
+        
 def majority_of_139_nanoaod(wfi, record, report):
     ## check on the main error, and bypass the request
+    return []
+
+def majority_of_X( wfi, report, code):
+    for tname,tinfo in report.get('tasks',{}).items():
+        terror = tinfo.get('errors',{})
+        if terror:
+            dominant,main_error = dominant_error( terror, fraction = 0.7 )
+            if dominant and main_error == code:
+                print "the main errors codes are", per_code
+                print per_code
+    return []
+    
+def majority_of_71104(wfi, record, report):
+    all_bad = True
+    for tname,tinfo in report.get('tasks',{}).items():
+        terror = tinfo.get('errors',{})
+        if terror:
+            dominant,main_error = dominant_error( terror, fraction = 0.7 )
+            if dominant and main_error == '71104':
+                print "the main errors code is 71104"
+            else:
+                all_bad = False
+    
+    if all_bad:
+        print "go on with simple acdc?"
+    
     return []
 
 def rulor(spec=None, options=None):
@@ -33,6 +75,7 @@ def rulor(spec=None, options=None):
     ## a list of function with a given trace ( wfi, record, report) => (action dict list)
     rules = [
         majority_of_139_nanoaod,
+        majority_of_71104,
     ]
 
     for wfo in wfs:
@@ -52,7 +95,7 @@ def rulor(spec=None, options=None):
         ### a rule for on-going issue with memory in campaign ...
         acted = False
         for condition in rules:
-            acts = condition( wfi, record, report):
+            acts = condition( wfi, record, report)
             if acts:
                 print "list of actions being taken for",wfo.name
                 for a in acts:
