af2c735c682675f0f15f978e56b6719b15e19019
jean-roch <vlimant@cern.ch>
2019-04-18 17:05:44 +0200

    some acl setup

--- whatjob.sh
+++ whatjob.sh
@@ -14,5 +14,14 @@ for each in 0268 0269 0272 0273 0274 0275; do
     echo "############## RUNNING ######################"
     ssh  vocms$each "ps -e -f | grep Uni| grep -v grep"
 
-    ssh vocms$each python /data/unified/WmAgentScripts/Unified/deadlock.py
+    ssh vocms$each "cd /data/unified/WmAgentScripts ; source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh ; python /data/unified/WmAgentScripts/Unified/deadlock.py"
+    
+    #ssh vocms$each "sudo chmod g+rw -R /data/unified/"
+    #ssh vocms$each "sudo chmod g+rw -R /data/unified-cache/"
+    #ssh vocms$each "sudo chmod g+rw -R /data/unified/WmAgentScripts/"
+    #ssh vocms$each "sudo chown -R :zh /data/unified/"
+    #ssh vocms$each "sudo chown -R :zh /data/unified-cache/"
+    ## relevant
+    #ssh vocms$each "sudo setfacl -R -m group:zh:rwx /data/unified/"
+    #ssh vocms$each "sudo setfacl -R -m group:zh:rwx /data/unified-cache/"
 done
