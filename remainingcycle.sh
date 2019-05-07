6c6fe486ad645a4667f624cd88aaa7877142b217
jean-roch <vlimant@cern.ch>
2019-05-07 09:09:17 +0200

    useless run

--- remainingcycle.sh
+++ remainingcycle.sh
@@ -5,7 +5,7 @@ lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
 source $BASE_DIR/cycle_common.sh $lock_name
 
 $BASE_DIR/cWrap.sh Unified/remainor.py parse True
-$BASE_DIR/cWrap.sh Unified/remainor.py go
+##$BASE_DIR/cWrap.sh Unified/remainor.py go
 
 rm -f $lock_name
 
