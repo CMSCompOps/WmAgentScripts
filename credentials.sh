f6b2096d721cccb3890086bfa468badff7e7d0e1
jean-roch <vlimant@cern.ch>
2019-05-07 10:03:37 +0200

    centralize in the right place

--- credentials.sh
+++ credentials.sh
@@ -25,5 +25,3 @@ else
     echo "using grid proxy" $X509_USER_PROXY
 fi
 
-export PYTHONPATH=$PYTHONPATH:/usr/lib64/python2.7/site-packages:/usr/lib/python2.7/site-packages
-source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
