e5bed43205eac508fbfec422ea705554ea9cba7b
jean-roch <vlimant@cern.ch>
2019-05-07 09:35:41 +0200

    centralizing

--- credentials.sh
+++ credentials.sh
@@ -4,12 +4,16 @@ export JIRA_SSO_COOKIE=/tmp/$USER-$HOSTNAME-vsdf43vfs
 if [ "$1" == "create" ] ; then
     echo "creating jira cookie" $JIRA_SSO_COOKIE
     cern-get-sso-cookie -u https://its.cern.ch/jira/loginCern.jsp -o $JIRA_SSO_COOKIE --krb
+else
+    echo "using jira cookie" $JIRA_SSO_COOKIE
 fi
 
 export MCM_SSO_COOKIE=/tmp/$USER-$HOSTNAME-vfsvdka573t
 if [ "$1" == "create" ] ; then
     echo "creating mcm cookie" $MCM_SSO_COOKIE
     cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o $MCM_SSO_COOKIE --krb
+else
+    echo "using mcm cookie" $MCM_SSO_COOKIE
 fi
 
 #talk to cmsweb
@@ -17,6 +21,9 @@ export X509_USER_PROXY=/tmp/$USER-4hty64k793hj
 if [ "$1" == "create" ] ; then
     echo "creating grid proxy" $X509_USER_PROXY
     cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 --rfc -pwstdin
+else
+    echo "using grid proxy" $X509_USER_PROXY
 fi
 
 export PYTHONPATH=$PYTHONPATH:/usr/lib64/python2.7/site-packages:/usr/lib/python2.7/site-packages
+source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
