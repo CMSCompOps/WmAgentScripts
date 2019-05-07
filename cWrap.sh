aea0275a987623376357bca2da52272c34836404
jean-roch <vlimant@cern.ch>
2019-05-07 10:03:37 +0200

    centralize in the right place

--- cWrap.sh
+++ cWrap.sh
@@ -42,6 +42,9 @@ echo $USER >> $log
 echo $HOSTNAME >> $log
 echo module $modulename>> $log 
 
+export PYTHONPATH=$PYTHONPATH:/usr/lib64/python2.7/site-packages:/usr/lib/python2.7/site-packages
+source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
+
 echo >> $log
 
 start=`date +%s`
