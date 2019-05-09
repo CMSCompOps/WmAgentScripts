0d6219c756677a2078e20ce1a152b81fc2051140
Sharad Agarwal <sharad.agarwal2014@vit.ac.in>
2019-05-09 15:49:25 +0200

    commented hepcloud conversion

--- Unified/addHoc.py
+++ Unified/addHoc.py
@@ -41,11 +41,11 @@ if N_for_cloud:
         if N_for_cloud<=0: break
         wfi = workflowInfo(reqmgr_url, wf['RequestName'], request = wf )
         print "testing",wf['RequestName']
-        if wfi.isGoodToConvertToStepChain() and wfi.isGoodForNERSC(no_step=True) and N_for_cloud:
-            print "good to convert to step so that we get something for hepcloud on next round",wf['RequestName']
-            os.system('Unified/rejector.py --clone --comment "convert to step for hepcloud" %s'% wf['RequestName'])
+        #if wfi.isGoodToConvertToStepChain() and wfi.isGoodForNERSC(no_step=True) and N_for_cloud:
+        #    print "good to convert to step so that we get something for hepcloud on next round",wf['RequestName']
+        #    os.system('Unified/rejector.py --to_step --clone --comment "convert to step for hepcloud" %s'% wf['RequestName'])
             ## just do that once and be done with it
-            N_for_cloud-=1
+        #    N_for_cloud-=1
 
         
 
