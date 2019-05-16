6cd789ee47b73e3543ab5a8d6771b75fe91061cc
jean-roch <vlimant@cern.ch>
2019-05-16 12:00:05 +0200

    new options

--- remainingcycle.sh
+++ remainingcycle.sh
@@ -4,8 +4,7 @@ HTML_DIR=/var/www/html/unified/
 lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
 source $BASE_DIR/cycle_common.sh $lock_name
 
-$BASE_DIR/cWrap.sh Unified/remainor.py parse True
-##$BASE_DIR/cWrap.sh Unified/remainor.py go
+$BASE_DIR/cWrap.sh Unified/remainor.py -n 5
 
 rm -f $lock_name
 
