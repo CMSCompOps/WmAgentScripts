6a8a7bde2cc82cdade69fff2a00ff477257cf595
Thong Q. Nguyen <thongonary@users.noreply.github.com>
2019-05-22 10:31:35 +0200

    Time splitting to be tested

--- utils.py
+++ utils.py
@@ -7630,7 +7630,8 @@ class workflowInfo:
                                                                                                                                                      time_per_input_lumi / (60.*60.),
                                                                                                                                                      job_timeout), level='critical')
                             this_max_events_per_lumi = int( job_target*60.*60. / timeperevent)
-                            max_events_per_lumi.append( this_max_events_per_lumi ) # For time constraint, don't take filter efficiency into account (B/c it's already factored in TimePerEvent?)
+                            max_events_per_lumi.append( this_max_events_per_lumi / efficiency_factor) # to be tested
+
                         else:
                             pass
 
