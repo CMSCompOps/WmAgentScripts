589f33c8c17bd922130615db8458a744c70578d9
sharad1126 <sharad.agarwal2014@vit.ac.in>
2019-07-05 12:02:06 +0200

    script to remove campign and put in archive

---
+++ remove_campaign.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import socket
+import json
+import optparse
+from json import JSONEncoder
+
+
+parser = optparse.OptionParser()
+parser.add_option('-n','--name',help='campaign name to be removed')
+(options,args) = parser.parse_args()
+
+if options.name:
+	with open('campaigns.json','r') as campaigns:
+		campaign_db = json.load(campaigns)
+                for key,value in campaign_db.items():
+                	if key == options.name:
+                                # remove the campaign from the mongoDB
+	                        remove_from_campaign_config = 'python campaignsConfiguration.py --name ' + options.name	+ ' --remove'
+	                        os.system(remove_from_campaign_config)
+                        	with open('archive_campaigns.json','r') as outfile:   
+                                	destination = json.load(outfile)	
+                                	destination.update({key : value})
+					with open('archive_campaigns.json', 'w') as out:
+                                        	json.dump(destination, out, indent=2) #putting the campaign into archive_campaigns.json file
+                                                with open('campaigns.json','w') as Origin:
+							del campaign_db[key] #deleting the campaign from campaigns.json file
+							Origin.write(json.dumps(campaign_db, indent=2)) 
+                        else:
+				print "campaign name not found"
+else:
+	print "enter a campaign name with -n <campaign name>"
+
