8d0b14ecfbe2bc3091cbf684b57a8eeedf312a71
Thong <thongnguyen1292@gmail.com>
2019-10-05 20:01:56 +0200

    Remove personal file

--- setupThong.sh
+++
@@ -1,5 +0,0 @@
-voms-proxy-init -voms cms
-export X509_USER_PROXY=/tmp/x509up_u82218
-export export PYTHONPATH=$PYTHONPATH:/usr/lib64/python2.7/site-packages:/usr/lib/python2.7/site-packages
-source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh
-#source credentials.sh
