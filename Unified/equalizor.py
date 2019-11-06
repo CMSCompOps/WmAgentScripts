35689305fb18d56599e24c15a212f510d4acd305
Thong <thongnguyen1292@gmail.com>
2019-11-06 01:24:39 +0100

    Overflow for wfs with remote PU read

--- Unified/equalizor.py
+++ Unified/equalizor.py
@@ -559,16 +559,17 @@ def equalizor(url , specific = None, options=None):
         mem = wfi.getMemory()
         memory_allowed = SI.sitesByMemory( mem , maxCore=ncores)
 
-        if not lhe and not prim and not sec and not wfi.isRelval():
-            ## no input at all: go for OSG!!!
-            print "adding", wfo.name, " addhoc for OSG and no task of the workflow requires any input"
-            add_to[wfo.name] = ['T3_US_OSG']
-            add_to[wfo.name] = ['T3_US_Colorado']
-            add_to[wfo.name] = ['T3_US_NERSC']
-            add_to[wfo.name] = ['T3_US_PSC']
-            add_to[wfo.name] = ['T3_US_TACC']
-            add_to[wfo.name] = ['T3_US_SDSC']
-            add_to[wfo.name] = ['T3_CH_CERN_HelixNebula']
+        if not lhe and not prim and not wfi.isRelval():
+            if (sec and wfi.request['TrustPUSitelists']) or (not sec):
+                ## no input at all: go for HPC!!!
+                print ("Adding {} adhoc for HPC as no task of the workflow requires any input".format(wfo.name))
+                add_to[wfo.name] = ['T3_US_OSG']
+                add_to[wfo.name] = ['T3_US_Colorado']
+                add_to[wfo.name] = ['T3_US_NERSC']
+                add_to[wfo.name] = ['T3_US_PSC']
+                add_to[wfo.name] = ['T3_US_TACC']
+                add_to[wfo.name] = ['T3_US_SDSC']
+                add_to[wfo.name] = ['T3_CH_CERN_HelixNebula']
 
         ## check needs override
         needs_overide = False
