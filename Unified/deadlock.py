37e7f2b21c03348306b2fbdfd38f604e0a2d21c9
jean-roch <vlimant@cern.ch>
2019-12-04 11:06:22 +0100

    trace

--- Unified/deadlock.py
+++ Unified/deadlock.py
@@ -2,15 +2,18 @@ import glob
 import os
 import socket
 
+print("cleaning unified locks")
 from utils import UnifiedLock
 UL = UnifiedLock(acquire=False)
 UL.deadlock()
 
+print("cleaning module locks")
 ## get rid of deadlock in mongodb
 from utils import moduleLock
 mlock = moduleLock(component='deadlock')
 mlock.check()
 
+print("purging cache info")
 from utils import cacheInfo
 cache = cacheInfo()
 cache.purge()
