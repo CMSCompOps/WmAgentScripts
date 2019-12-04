c177b4f7cf042fc8c75b75f59e82cb8766c457d8
jean-roch <vlimant@cern.ch>
2019-12-04 11:20:12 +0100

    proper throttling

--- assigncycle.sh
+++ assigncycle.sh
@@ -12,19 +12,19 @@ $BASE_DIR/cWrap.sh Unified/batchor.py
 
 ## this could replace stagor in much faster
 ## needs to put some work in stagor or not ?
-$BASE_DIR/cWrap.sh Unified/assignor.py --early --limit 50
+$BASE_DIR/cWrap.sh Unified/assignor.py --early
 
 ## get the workflow in/out the system
 $BASE_DIR/cWrap.sh Unified/injector.py
 
 ## assigned those that could have passed through directly
-$BASE_DIR/cWrap.sh Unified/assignor.py --from_status staged --limit 50
+$BASE_DIR/cWrap.sh Unified/assignor.py --from_status staged
 
-$BASE_DIR/cWrap.sh Unified/assignor.py _PR_newco --limit 50
-$BASE_DIR/cWrap.sh Unified/assignor.py _PR_ref --limit 50
+$BASE_DIR/cWrap.sh Unified/assignor.py _PR_newco
+$BASE_DIR/cWrap.sh Unified/assignor.py _PR_ref
 
 ## assign the workflow to sites
-$BASE_DIR/cWrap.sh Unified/assignor.py --limit 50
+$BASE_DIR/cWrap.sh Unified/assignor.py
 
 rm -f $lock_name
 
