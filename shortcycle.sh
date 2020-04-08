cb3cd9f70e3427a0fb6e6bd5dc30db54642138d1
sharad1126 <sharad.agarwal2014@vit.ac.in>
2020-04-08 11:21:38 +0200

    mappor added to run before equalizor

--- shortcycle.sh
+++ shortcycle.sh
@@ -4,6 +4,9 @@ HTML_DIR=/var/www/html/unified/
 lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
 source $BASE_DIR/cycle_common.sh $lock_name
 
+##mapping the sites
+$BASE_DIR/cWrap.sh Unified/mappor.py
+
 ## equalize site white list at the condor level                                                                                      
 $BASE_DIR/cWrap.sh Unified/equalizor.py
 
