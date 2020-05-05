b6e41185e8415825862022e5308eaf765b2200c8
Dmytro Kovalskyi <dmytro@cern.ch>
2020-05-05 11:29:02 +0200

    added command line option

--- Unified/step_chain_test.py
+++ Unified/step_chain_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from utils import getWorkflows, getWorkflowById, getWorkLoad, componentInfo, sendEmail, workflowInfo, sendLog, reqmgr_url, getDatasetStatus, unifiedConfiguration, moduleLock, do_html_in_each_module, getDatasetFiles
+import sys
 
 def step_chain_test(url, wf):
     transform_keywords = None
@@ -9,12 +10,15 @@ def step_chain_test(url, wf):
 
 if __name__ == "__main__":
     url = reqmgr_url
-    wfs = [
-        'pdmvserv_task_TSG-Run3Winter20wmLHEGS-00017__v1_T_200408_150110_9739',
-        'pdmvserv_task_B2G-RunIIFall17wmLHEGS-01908__v1_T_200423_040350_2140',
-        'pdmvserv_task_B2G-RunIISummer15wmLHEGS-03734__v1_T_200414_195359_2458',
-        'pdmvserv_task_SMP-RunIISummer15wmLHEGS-00391__v1_T_200417_104228_4225'
-    ]
+    if len(sys.argv)==2:
+        wfs = [ sys.argv[1] ]
+    else:
+        wfs = [
+            'pdmvserv_task_TSG-Run3Winter20wmLHEGS-00017__v1_T_200408_150110_9739',
+            'pdmvserv_task_B2G-RunIIFall17wmLHEGS-01908__v1_T_200423_040350_2140',
+            'pdmvserv_task_B2G-RunIISummer15wmLHEGS-03734__v1_T_200414_195359_2458',
+            'pdmvserv_task_SMP-RunIISummer15wmLHEGS-00391__v1_T_200417_104228_4225'
+        ]
     for wf in wfs:
         step_chain_test(url,wf)
     
