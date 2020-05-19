e53186c7cabc066fce82d99e63e14535d6c301af
sharad1126 <sharad.agarwal2014@vit.ac.in>
2020-05-19 11:36:52 +0200

    added rucio export path

--- postcycle-review-recovering.sh
+++ postcycle-review-recovering.sh
@@ -1,6 +1,9 @@
 BASE_DIR=/data/unified/WmAgentScripts/
 HTML_DIR=/var/www/html/unified/
 
+## export RUCIO_HOME
+export RUCIO_HOME=~/.local/
+
 lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
 source $BASE_DIR/cycle_common.sh $lock_name
 
