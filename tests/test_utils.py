7bf701eb2dcd53e0188f0edad929e39fdc3b5a5b
Ishan Rai <ishanrai05@gmail.com>
2020-07-25 08:49:33 +0530

    add unittests for utils.UnifiedConfiguration

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,15 @@
 import sys
 
 import unittest
-from mock import MagicMock
+import mock
+from mock import MagicMock, patch, mock_open
+import json
 
 
 class TestDeepUpdate(unittest.TestCase):
 
     def setUp(self):
+
         self.u = {
             "first": {"a": "A"},
             "second": "B"
@@ -25,6 +28,36 @@ class TestDeepUpdate(unittest.TestCase):
 
         self.assertDictEqual(self.d, self.deep_update(self.d, self.u))
 
+
+class TestUnifiedConfiguration(unittest.TestCase):
+
+    def setUp(self):
+        self.test_json = json.dumps({
+            "email": {
+                "value": ["test@cern.ch", "test@testmail.com"],
+                "description": "The list of people that get the emails notifications"
+            }
+        })
+
+    def test_get_json(self):
+
+        from WmAgentScripts.utils import unifiedConfiguration, open_json_file
+        mock_open_json = mock_open(read_data=self.test_json)
+        with patch('__builtin__.open', mock_open_json):
+            result = open_json_file('filename')
+            uc = unifiedConfiguration(configFile='fake_file_path')
+            self.assertEqual(
+                uc.get("email"), ["test@cern.ch", "test@testmail.com"])
+
+        with self.assertRaises(SystemExit):
+            self.assertEqual(
+                uc.get("cernmails"), [
+                    "test@cern.ch", "test@testmail.com"])
+
+    def test_get_mongodb(self):
+        
+        # TODO: mock db and write tests
+        pass
 
 
 if __name__ == '__main__':
