aba827f4c14a4836ed68e77a99a354d5731a627e
sharad1126 <sharad.agarwal2014@vit.ac.in>
2020-08-05 11:14:39 +0200

    log should get output dataset name instead of int value

--- Unified/checkor.py
+++ Unified/checkor.py
@@ -1278,7 +1278,7 @@ class CheckBuster(threading.Thread):
             if custodial:
                 for output in out_worth_checking:
                     if getDatasetSize(output)/1023 > tape_size_limit:
-                        wfi.sendLog('checkor',"%s output size (%s TB) is too large for the limit set (%s TB)"%( output, getDatasetSize(output)/1023, tape_size_limit))
+                        wfi.sendLog('checkor',"%s output size (%s TB) is too large for the limit set (%s TB)"%( output, out_worth_checking[output], tape_size_limit))
                         assistance_tags.add('bigoutput')
                         custodial = None
 
