72699a8d8fe375ea0529e9a916e72fab49d76abf
sharad1126 <sharad.agarwal2014@vit.ac.in>
2020-08-05 11:09:24 +0200

    size_worth_checking can't have unicode characters as the list is made of int

--- Unified/checkor.py
+++ Unified/checkor.py
@@ -1277,8 +1277,8 @@ class CheckBuster(threading.Thread):
 
             if custodial:
                 for output in out_worth_checking:
-                    if size_worth_checking[output] > tape_size_limit:
-                        wfi.sendLog('checkor',"%s output size (%s TB) is too large for the limit set (%s TB)"%( output, size_worth_checking[output], tape_size_limit))
+                    if getDatasetSize(output)/1023 > tape_size_limit:
+                        wfi.sendLog('checkor',"%s output size (%s TB) is too large for the limit set (%s TB)"%( output, getDatasetSize(output)/1023, tape_size_limit))
                         assistance_tags.add('bigoutput')
                         custodial = None
 
