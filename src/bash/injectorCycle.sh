8837cc6ef1a7f367aed61216cb67a0b88af93f35
haozturk <haozturk@cern.ch>
2022-01-17 12:15:31 +0300

    Introduce a script to orchestrate injector cycle

---
+++ src/bash/injectorCycle.sh
@@ -0,0 +1,11 @@
+# Set up the environment
+source /data/unifiedPy3/setUnifiedPy3Env.sh
+
+# Checkout to the proper branch
+git checkout python3-migration
+
+# Set up the permissions
+chmod +x src/bash/*.sh
+
+# Run the script
+source src/bash/runner.sh src/python/Unified/Injector.py
