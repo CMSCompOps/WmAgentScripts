ec00bc59f08128048aa7ac63b78eed57943957ec
haozturk <haozturk@cern.ch>
2022-09-22 13:34:12 +0300

    Set the proper environment before every actcycle run

--- actcycle.sh
+++ actcycle.sh
@@ -4,8 +4,8 @@ HTML_DIR=/var/www/html/unified/
 lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
 source $BASE_DIR/cycle_common.sh $lock_name
 
-## get sso cookie and new grid proxy
-source $BASE_DIR/credentials.sh
+## Set up the environment
+source /data/unifiedPy3-fast/setEnv.sh
 
 ## submit ACDCs and clones from actions submitted via new recovery tools
 $BASE_DIR/cWrap.sh Unified/actor.py
