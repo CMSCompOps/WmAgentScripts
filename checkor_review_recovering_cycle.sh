058e4c5b57c811ece02bddaa3400509e6dc6b148
haozturk <haozturk@cern.ch>
2023-01-11 11:56:46 +0100

    Introduce checkor cycle script

---
+++ checkor_review_recovering_cycle.sh
@@ -0,0 +1,13 @@
+BASE_DIR=/data/unifiedPy3-fast/WmAgentScripts
+HTML_DIR=/var/www/html/unified/
+
+lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
+source $BASE_DIR/cycle_common.sh $lock_name
+
+## Set up the environment
+source /data/unifiedPy3-fast/setEnv.sh
+
+## submit ACDCs and clones from actions submitted via new recovery tools
+$BASE_DIR/cWrap.sh Unified/checkor.py  --review --recovering --threads 5
+
+rm -f $lock_name
