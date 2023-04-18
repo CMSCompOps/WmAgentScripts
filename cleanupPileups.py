9a871c175dfc1aa44dd84495672de4c409897842
haozturk <haozturk@cern.ch>
2023-04-18 12:28:59 +0200

    Check consistency of pileup between Rucio and campaign config

--- cleanupPileups.py
+++ cleanupPileups.py
@@ -8,18 +8,40 @@ f = open('campaigns.json')
 
 campaigns = json.load(f)
 
+rucioClient = RucioClient()
+
 for campaignName, v in list(campaigns.items()):
-    print (campaignName)
+    #print (campaignName)
     if "secondaries" in v:
         for secondaryName, locationsDict in list(v["secondaries"].items()):
-            print (secondaryName)
-            print (locationsDict)
+            #print (secondaryName)
+            #print (locationsDict)
+
+            secondaryLocations = []
+            if "SecondaryLocation" in locationsDict:
+                secondaryLocations = locationsDict["SecondaryLocation"]
+            elif "SiteWhitelist" in locationsDict:
+                secondaryLocations = locationsDict["SiteWhitelist"]
+            else:
+                print ("No location defined for the secondary, exiting")
+
+            pileup_locations_on_rucio = rucioClient.getDatasetLocationsByAccount(secondaryName, "wmcore_transferor")
+
+            if set(pileup_locations_on_rucio) != set(secondaryLocations):
+                print("Inconsistency of pileup between Rucio and campaign config")
+                print(campaignName)
+                print(secondaryName)
+                print("On campaign config:", str(secondaryLocations) )
+                print("On Rucio:", str(pileup_locations_on_rucio))
 
 
 
 
 
-#rucioClient = RucioClient()
 
 
-#pileup_locations = rucioClient.getDatasetLocationsByAccount(sec, "wmcore_transferor")
+
+#
+
+
+#
