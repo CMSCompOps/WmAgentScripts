414c19e9ebbcd83a45f6051aaa5cacc3db7334ac
haozturk <haozturk@cern.ch>
2023-08-10 13:06:28 +0200

    Update McM SSO auth

--- src/bash/authenticate.sh
+++ src/bash/authenticate.sh
@@ -2,7 +2,8 @@ export JIRA_SSO_COOKIE=/tmp/$USER-$HOSTNAME-vsdf43vfs
 cern-get-sso-cookie -u https://its.cern.ch/jira/loginCern.jsp -o $JIRA_SSO_COOKIE --krb
 
 export MCM_SSO_COOKIE=/tmp/$USER-$HOSTNAME-vfsvdka573t
-cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o $MCM_SSO_COOKIE --krb
+export REQUESTS_CA_BUNDLE="/etc/pki/tls/certs/ca-bundle.trust.crt"
+auth-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o $MCM_SSO_COOKIE
 
 export X509_USER_PROXY=/tmp/$USER-4hty64k793hj
 cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 --rfc -pwstdin
